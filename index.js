eb31a54467d00d713dc1120f7b5bc4ffc04a26a3
tushardhole <tushardhole@hotmail.com>
2018-01-17 19:54:24 +0530

    feat: implement valid-expect-in-promise rule (#42)
    * feat: implement valid-expect-in-promise rule
    
    introduce a lint rule to report error when testing promises. If a expectation is added inside a then or catch block of promise then as per jest docs,
    it is mandatory to return that promise for succesfull execution of that expectation. This rule will report all such expectations where promise is not returned.
    
    * fix: do not add the rule to recommended
    
    * fix: do not validate await expressions for valid_expect_in_promise
    
    * fix: scenario with expect in nested then/catch
    
    * docs: updating docs for valid-expect-in-promise rule
    
    * test: add failing test case
    
    * fix: scenario where promise is returned later
    
    * fix: adding more scenarios where promise is returned later
    
    * test: adding more tests
    
    * fix: adding scenarios for non test functions
    
    * fix: refactor to avoid multiple execution of getTestFuncBody
    
    * fix: scenario with arrow-short-hand-fn with implicit return statement
    
    * fix: scenario with multiline function in then block
    
    * fix: scenario with short hand arrow function in then block
    
    * fix: do not validate tests with done async param
    
    * fix: scenario where expect in then is preceded by return
    
    * fix: duplicate warning for same promise
    
    * fix: better naming
    
    * test: better formatting

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const preferToBeUndefined = require('./rules/prefer_to_be_undefined');
 const preferToHaveLength = require('./rules/prefer_to_have_length');
 const validExpect = require('./rules/valid_expect');
 const preferExpectAssertions = require('./rules/prefer_expect_assertions');
+const validExpectInPromise = require('./rules/valid_expect_in_promise');
 
 const snapshotProcessor = require('./processors/snapshot-processor');
 
@@ -64,5 +65,6 @@ module.exports = {
     'prefer-to-have-length': preferToHaveLength,
     'valid-expect': validExpect,
     'prefer-expect-assertions': preferExpectAssertions,
+    'valid-expect-in-promise': validExpectInPromise,
   },
 };
