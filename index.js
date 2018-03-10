fb54759139f15a48319e7fec297add5aed9ab4a1
Ran Yitzhaki <ranyitz@users.noreply.github.com>
2018-03-10 20:09:28 +0200

    feat(rules): add 'no-jest-import' rule to the recommended config (#99)
    Fixes #98

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ module.exports = {
         'jest/no-disabled-tests': 'warn',
         'jest/no-focused-tests': 'error',
         'jest/no-identical-title': 'error',
+        'jest/no-jest-import': 'warn',
         'jest/prefer-to-have-length': 'warn',
         'jest/valid-expect': 'error',
       },
