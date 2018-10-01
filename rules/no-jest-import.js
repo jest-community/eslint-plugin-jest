2a1ae170d8f616033518c1585b40ff31c471253e
mackie <macklinu@users.noreply.github.com>
2018-10-01 12:40:33 -0700

    refactor(no-jest-import): use ESQuery syntax for selectors (#158)

--- rules/no-jest-import.js
+++ rules/no-jest-import.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const getDocsUrl = require('./util').getDocsUrl;
-const getNodeName = require('./util').getNodeName;
+
 const message = `Jest is automatically in scope. Do not import "jest", as Jest doesn't export anything.`;
 
 module.exports = {
@@ -12,32 +12,14 @@ module.exports = {
   },
   create(context) {
     return {
-      ImportDeclaration(node) {
-        if (node.source.value === 'jest') {
-          context.report({
-            node,
-            message,
-          });
-        }
+      'ImportDeclaration[source.value="jest"]'(node) {
+        context.report({ node, message });
       },
-      CallExpression(node) {
-        const calleeName = getNodeName(node.callee);
-        if (
-          calleeName === 'require' &&
-          node.arguments[0] &&
-          node.arguments[0].value === 'jest'
-        ) {
-          context.report({
-            loc: {
-              end: {
-                column: node.arguments[0].loc.end.column,
-                line: node.arguments[0].loc.end.line,
-              },
-              start: node.arguments[0].loc.start,
-            },
-            message,
-          });
-        }
+      'CallExpression[callee.name="require"][arguments.0.value="jest"]'(node) {
+        context.report({
+          loc: node.arguments[0].loc,
+          message,
+        });
       },
     };
   },
