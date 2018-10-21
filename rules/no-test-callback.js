2ac472ca0e8f91cc70b16f29bc6941ff392628b7
Simen Bekkhus <sbekkhus91@gmail.com>
2018-10-21 21:08:04 +0200

    refactor: cleanup

--- rules/no-test-callback.js
+++ rules/no-test-callback.js
@@ -19,60 +19,58 @@ module.exports = {
 
         const callback = node.arguments[1];
 
-        if (callback.params.length === 1) {
-          const argument = callback.params[0];
-          context.report({
-            node: argument,
-            message: 'Illegal usage of test callback',
-            fix(fixer) {
-              const sourceCode = context.getSourceCode();
-              const body = callback.body;
-              const firstBodyToken = sourceCode.getFirstToken(body);
-              const lastBodyToken = sourceCode.getLastToken(body);
-              const tokenBeforeArgument = sourceCode.getTokenBefore(argument);
-              const tokenAfterArgument = sourceCode.getTokenAfter(argument);
-              const argumentInParens =
-                tokenBeforeArgument.value === '(' &&
-                tokenAfterArgument.value === ')';
-
-              let argumentFix = fixer.replaceText(argument, '()');
-
-              if (argumentInParens) {
-                argumentFix = fixer.remove(argument);
-              }
-
-              let newCallback = argument.name;
-
-              if (argumentInParens) {
-                newCallback = `(${newCallback})`;
-              }
-
-              if (body.type === 'BlockStatement') {
-                let beforeReplacement = `new Promise(${newCallback} => {`;
-                if (callback.async) {
-                  beforeReplacement = `await ${beforeReplacement}`;
-                } else {
-                  beforeReplacement = `return ${beforeReplacement}`;
-                }
-
-                return [
-                  argumentFix,
-                  fixer.insertTextAfter(firstBodyToken, beforeReplacement),
-                  fixer.insertTextAfter(lastBodyToken, ')}'),
-                ];
-              } else {
-                return [
-                  argumentFix,
-                  fixer.insertTextBefore(
-                    firstBodyToken,
-                    `new Promise(${newCallback} => `
-                  ),
-                  fixer.insertTextAfter(lastBodyToken, ')'),
-                ];
-              }
-            },
-          });
+        if (callback.params.length !== 1) {
+          return;
         }
+
+        const argument = callback.params[0];
+        context.report({
+          node: argument,
+          message: 'Illegal usage of test callback',
+          fix(fixer) {
+            const sourceCode = context.getSourceCode();
+            const body = callback.body;
+            const firstBodyToken = sourceCode.getFirstToken(body);
+            const lastBodyToken = sourceCode.getLastToken(body);
+            const tokenBeforeArgument = sourceCode.getTokenBefore(argument);
+            const tokenAfterArgument = sourceCode.getTokenAfter(argument);
+            const argumentInParens =
+              tokenBeforeArgument.value === '(' &&
+              tokenAfterArgument.value === ')';
+
+            let argumentFix = fixer.replaceText(argument, '()');
+
+            if (argumentInParens) {
+              argumentFix = fixer.remove(argument);
+            }
+
+            let newCallback = argument.name;
+
+            if (argumentInParens) {
+              newCallback = `(${newCallback})`;
+            }
+
+            let beforeReplacement = `new Promise(${newCallback} => `;
+            let afterReplacement = ')';
+            let replaceBefore = true;
+
+            if (body.type === 'BlockStatement') {
+              const keyword = callback.async ? 'await' : 'return';
+
+              beforeReplacement = `${keyword} ${beforeReplacement}{`;
+              afterReplacement += '}';
+              replaceBefore = false;
+            }
+
+            return [
+              argumentFix,
+              replaceBefore
+                ? fixer.insertTextBefore(firstBodyToken, beforeReplacement)
+                : fixer.insertTextAfter(firstBodyToken, beforeReplacement),
+              fixer.insertTextAfter(lastBodyToken, afterReplacement),
+            ];
+          },
+        });
       },
     };
   },
