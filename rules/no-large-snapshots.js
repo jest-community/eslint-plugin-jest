9137c2132c1e2ac88f5ada26fa6856669e28cf3b
JohanBrorson <JohanBrorson@users.noreply.github.com>
2018-10-24 14:27:26 +0200

    fix(no-large-snapshots): support inline snapshots (#186)

--- rules/no-large-snapshots.js
+++ rules/no-large-snapshots.js
@@ -2,6 +2,27 @@
 
 const getDocsUrl = require('./util').getDocsUrl;
 
+const reportOnViolation = (context, node) => {
+  const lineLimit =
+    context.options[0] && Number.isFinite(context.options[0].maxSize)
+      ? context.options[0].maxSize
+      : 50;
+  const startLine = node.loc.start.line;
+  const endLine = node.loc.end.line;
+  const lineCount = endLine - startLine;
+
+  if (lineCount > lineLimit) {
+    context.report({
+      message:
+        lineLimit === 0
+          ? 'Expected to not encounter a Jest snapshot but was found with {{ lineCount }} lines long'
+          : 'Expected Jest snapshot to be smaller than {{ lineLimit }} lines but was {{ lineCount }} lines long',
+      data: { lineLimit, lineCount },
+      node,
+    });
+  }
+};
+
 module.exports = {
   meta: {
     docs: {
@@ -10,26 +31,21 @@ module.exports = {
   },
   create(context) {
     if (context.getFilename().endsWith('.snap')) {
-      const lineLimit =
-        context.options[0] && Number.isFinite(context.options[0].maxSize)
-          ? context.options[0].maxSize
-          : 50;
-
       return {
         ExpressionStatement(node) {
-          const startLine = node.loc.start.line;
-          const endLine = node.loc.end.line;
-          const lineCount = endLine - startLine;
-
-          if (lineCount > lineLimit) {
-            context.report({
-              message:
-                lineLimit === 0
-                  ? 'Expected to not encounter a Jest snapshot but was found with {{ lineCount }} lines long'
-                  : 'Expected Jest snapshot to be smaller than {{ lineLimit }} lines but was {{ lineCount }} lines long',
-              data: { lineLimit, lineCount },
-              node,
-            });
+          reportOnViolation(context, node);
+        },
+      };
+    } else if (context.getFilename().endsWith('.js')) {
+      return {
+        CallExpression(node) {
+          const propertyName =
+            node.callee.property && node.callee.property.name;
+          if (
+            propertyName === 'toMatchInlineSnapshot' ||
+            propertyName === 'toThrowErrorMatchingInlineSnapshot'
+          ) {
+            reportOnViolation(context, node);
           }
         },
       };
