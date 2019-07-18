b8847e8835364f45b27241126f379e720aabcd2f
Mark <nimalan.m@protonmail.com>
2019-07-19 02:20:04 +0530

    chore: migrate valid-describe to ts (#308)

--- src/rules/tsUtils.ts
+++ src/rules/tsUtils.ts
@@ -1,6 +1,10 @@
 // TODO: rename to utils.ts when TS migration is complete
 import { basename } from 'path';
-import { ESLintUtils } from '@typescript-eslint/experimental-utils';
+import {
+  AST_NODE_TYPES,
+  ESLintUtils,
+  TSESTree,
+} from '@typescript-eslint/experimental-utils';
 import { version } from '../../package.json';
 
 const REPO_URL = 'https://github.com/jest-community/eslint-plugin-jest';
@@ -9,3 +13,27 @@ export const createRule = ESLintUtils.RuleCreator(name => {
   const ruleName = basename(name, '.ts');
   return `${REPO_URL}/blob/v${version}/docs/rules/${ruleName}.md`;
 });
+
+enum DescribeAlias {
+  'describe',
+  'fdescribe',
+  'xdescribe',
+}
+
+export type FunctionExpression =
+  | TSESTree.ArrowFunctionExpression
+  | TSESTree.FunctionExpression;
+
+export const isFunction = (node: TSESTree.Node): node is FunctionExpression =>
+  node.type === AST_NODE_TYPES.FunctionExpression ||
+  node.type === AST_NODE_TYPES.ArrowFunctionExpression;
+
+export const isDescribe = (node: TSESTree.CallExpression): boolean => {
+  return (
+    (node.callee.type === AST_NODE_TYPES.Identifier &&
+      node.callee.name in DescribeAlias) ||
+    (node.callee.type === AST_NODE_TYPES.MemberExpression &&
+      node.callee.object.type === AST_NODE_TYPES.Identifier &&
+      node.callee.object.name in DescribeAlias)
+  );
+};
