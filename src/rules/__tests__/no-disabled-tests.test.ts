86d92211262890356a388a03fe15bfe82171dc40
Gareth Jones <jones258@gmail.com>
2019-07-19 18:11:56 +1200

    chore: convert `no-disabled-tests` rule to TypeScript

--- src/rules/__tests__/no-disabled-tests.test.ts
+++ src/rules/__tests__/no-disabled-tests.test.ts
@@ -1,7 +1,7 @@
-import { RuleTester } from 'eslint';
+import { TSESLint } from '@typescript-eslint/experimental-utils';
 import rule from '../no-disabled-tests';
 
-const ruleTester = new RuleTester({
+const ruleTester = new TSESLint.RuleTester({
   parserOptions: {
     sourceType: 'module',
   },
