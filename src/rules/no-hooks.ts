5a718fbe3d17d23d1dc5aa8bdc8cc8ec699598c5
Gareth Jones <jones258@gmail.com>
2019-07-20 22:02:04 +1200

    chore(no-hooks): convert to typescript

---
+++ src/rules/no-hooks.ts
@@ -0,0 +1,50 @@
+import { HookName, createRule, isHook } from './tsUtils';
+
+export default createRule({
+  name: __filename,
+  meta: {
+    docs: {
+      category: 'Best Practices',
+      description: 'Disallow setup and teardown hooks',
+      recommended: false,
+    },
+    messages: {
+      unexpectedHook: "Unexpected '{{ hookName }}' hook",
+    },
+    schema: [
+      {
+        type: 'object',
+        properties: {
+          allow: {
+            type: 'array',
+            contains: ['beforeAll', 'beforeEach', 'afterAll', 'afterEach'],
+          },
+        },
+        additionalProperties: false,
+      },
+    ],
+    type: 'suggestion',
+  },
+  defaultOptions: [{ allow: [] } as { allow: readonly HookName[] }],
+  create(context, [{ allow }]) {
+    const whitelistedHookNames = allow.reduce((hashMap, value) => {
+      hashMap[value] = true;
+      return hashMap;
+    }, Object.create(null));
+
+    const isWhitelisted = (node: { callee: { name: string } }) =>
+      whitelistedHookNames[node.callee.name];
+
+    return {
+      CallExpression(node) {
+        if (isHook(node) && !isWhitelisted(node)) {
+          context.report({
+            node,
+            messageId: 'unexpectedHook',
+            data: { hookName: node.callee.name },
+          });
+        }
+      },
+    };
+  },
+});
