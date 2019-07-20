be4e47848bb38285b4daf770b75891743a0ef950
Gareth Jones <Jones258@Gmail.com>
2019-07-20 20:54:14 +1200

    chore(prefer-inline-snapshots): convert to typescript (#319)

---
+++ src/rules/prefer-inline-snapshots.ts
@@ -0,0 +1,60 @@
+import { AST_NODE_TYPES } from '@typescript-eslint/experimental-utils';
+import { createRule } from './tsUtils';
+
+export default createRule({
+  name: __filename,
+  meta: {
+    docs: {
+      category: 'Best Practices',
+      description: 'Suggest using inline snapshots',
+      recommended: false,
+    },
+    messages: {
+      toMatch: 'Use toMatchInlineSnapshot() instead',
+      toMatchError: 'Use toThrowErrorMatchingInlineSnapshot() instead',
+    },
+    fixable: 'code',
+    schema: [],
+    type: 'suggestion',
+  },
+  defaultOptions: [],
+  create(context) {
+    return {
+      CallExpression(node) {
+        const { callee } = node;
+
+        if (
+          callee.type !== AST_NODE_TYPES.MemberExpression ||
+          callee.property.type !== AST_NODE_TYPES.Identifier
+        ) {
+          return;
+        }
+
+        if (callee.property.name === 'toMatchSnapshot') {
+          context.report({
+            fix(fixer) {
+              return [
+                fixer.replaceText(callee.property, 'toMatchInlineSnapshot'),
+              ];
+            },
+            messageId: 'toMatch',
+            node: callee.property,
+          });
+        } else if (callee.property.name === 'toThrowErrorMatchingSnapshot') {
+          context.report({
+            fix(fixer) {
+              return [
+                fixer.replaceText(
+                  callee.property,
+                  'toThrowErrorMatchingInlineSnapshot',
+                ),
+              ];
+            },
+            messageId: 'toMatchError',
+            node: callee.property,
+          });
+        }
+      },
+    };
+  },
+});
