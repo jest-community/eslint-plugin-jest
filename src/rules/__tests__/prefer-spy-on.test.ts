4200e769900f09ba1d1552635fb9ca138e2bcded
Gareth Jones <Jones258@Gmail.com>
2019-07-20 23:53:10 +1200

    chore(prefer-spy-on): migrate to TS (#326)

--- src/rules/__tests__/prefer-spy-on.test.ts
+++ src/rules/__tests__/prefer-spy-on.test.ts
@@ -1,7 +1,10 @@
-import { RuleTester } from 'eslint';
+import {
+  AST_NODE_TYPES,
+  TSESLint,
+} from '@typescript-eslint/experimental-utils';
 import rule from '../prefer-spy-on';
 
-const ruleTester = new RuleTester({
+const ruleTester = new TSESLint.RuleTester({
   parserOptions: {
     ecmaVersion: 6,
   },
@@ -22,44 +25,84 @@ ruleTester.run('prefer-spy-on', rule, {
   invalid: [
     {
       code: 'obj.a = jest.fn(); const test = 10;',
-      errors: [{ messageId: 'useJestSpyOn', type: 'AssignmentExpression' }],
+      errors: [
+        {
+          messageId: 'useJestSpyOn',
+          type: AST_NODE_TYPES.AssignmentExpression,
+        },
+      ],
       output: "jest.spyOn(obj, 'a'); const test = 10;",
     },
     {
       code: "Date['now'] = jest['fn']()",
-      errors: [{ messageId: 'useJestSpyOn', type: 'AssignmentExpression' }],
+      errors: [
+        {
+          messageId: 'useJestSpyOn',
+          type: AST_NODE_TYPES.AssignmentExpression,
+        },
+      ],
       output: "jest.spyOn(Date, 'now')",
     },
     {
       code: 'window[`${name}`] = jest[`fn`]()',
-      errors: [{ messageId: 'useJestSpyOn', type: 'AssignmentExpression' }],
+      errors: [
+        {
+          messageId: 'useJestSpyOn',
+          type: AST_NODE_TYPES.AssignmentExpression,
+        },
+      ],
       output: 'jest.spyOn(window, `${name}`)',
     },
     {
       code: "obj['prop' + 1] = jest['fn']()",
-      errors: [{ messageId: 'useJestSpyOn', type: 'AssignmentExpression' }],
+      errors: [
+        {
+          messageId: 'useJestSpyOn',
+          type: AST_NODE_TYPES.AssignmentExpression,
+        },
+      ],
       output: "jest.spyOn(obj, 'prop' + 1)",
     },
     {
       code: 'obj.one.two = jest.fn(); const test = 10;',
-      errors: [{ messageId: 'useJestSpyOn', type: 'AssignmentExpression' }],
+      errors: [
+        {
+          messageId: 'useJestSpyOn',
+          type: AST_NODE_TYPES.AssignmentExpression,
+        },
+      ],
       output: "jest.spyOn(obj.one, 'two'); const test = 10;",
     },
     {
       code: 'obj.a = jest.fn(() => 10)',
-      errors: [{ messageId: 'useJestSpyOn', type: 'AssignmentExpression' }],
+      errors: [
+        {
+          messageId: 'useJestSpyOn',
+          type: AST_NODE_TYPES.AssignmentExpression,
+        },
+      ],
       output: "jest.spyOn(obj, 'a').mockImplementation(() => 10)",
     },
     {
       code:
         "obj.a.b = jest.fn(() => ({})).mockReturnValue('default').mockReturnValueOnce('first call'); test();",
-      errors: [{ messageId: 'useJestSpyOn', type: 'AssignmentExpression' }],
+      errors: [
+        {
+          messageId: 'useJestSpyOn',
+          type: AST_NODE_TYPES.AssignmentExpression,
+        },
+      ],
       output:
         "jest.spyOn(obj.a, 'b').mockImplementation(() => ({})).mockReturnValue('default').mockReturnValueOnce('first call'); test();",
     },
     {
       code: 'window.fetch = jest.fn(() => ({})).one.two().three().four',
-      errors: [{ messageId: 'useJestSpyOn', type: 'AssignmentExpression' }],
+      errors: [
+        {
+          messageId: 'useJestSpyOn',
+          type: AST_NODE_TYPES.AssignmentExpression,
+        },
+      ],
       output:
         "jest.spyOn(window, 'fetch').mockImplementation(() => ({})).one.two().three().four",
     },
