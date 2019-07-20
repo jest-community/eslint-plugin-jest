a4bdeb31258afb6af2911799f1694982a22decd7
Gareth Jones <Jones258@Gmail.com>
2019-07-20 22:51:10 +1200

    chore(no-hooks): convert to typescript (#322)

---
+++ src/rules/__tests__/no-hooks.test.ts
@@ -0,0 +1,60 @@
+import { TSESLint } from '@typescript-eslint/experimental-utils';
+import rule from '../no-hooks';
+import { HookName } from '../tsUtils';
+
+const ruleTester = new TSESLint.RuleTester({
+  parserOptions: {
+    ecmaVersion: 6,
+  },
+});
+
+ruleTester.run('no-hooks', rule, {
+  valid: [
+    'test("foo")',
+    'describe("foo", () => { it("bar") })',
+    'test("foo", () => { expect(subject.beforeEach()).toBe(true) })',
+    {
+      code: 'afterEach(() => {}); afterAll(() => {});',
+      options: [{ allow: [HookName.afterEach, HookName.afterAll] }],
+    },
+  ],
+  invalid: [
+    {
+      code: 'beforeAll(() => {})',
+      errors: [
+        { messageId: 'unexpectedHook', data: { hookName: HookName.beforeAll } },
+      ],
+    },
+    {
+      code: 'beforeEach(() => {})',
+      errors: [
+        {
+          messageId: 'unexpectedHook',
+          data: { hookName: HookName.beforeEach },
+        },
+      ],
+    },
+    {
+      code: 'afterAll(() => {})',
+      errors: [
+        { messageId: 'unexpectedHook', data: { hookName: HookName.afterAll } },
+      ],
+    },
+    {
+      code: 'afterEach(() => {})',
+      errors: [
+        { messageId: 'unexpectedHook', data: { hookName: HookName.afterEach } },
+      ],
+    },
+    {
+      code: 'beforeEach(() => {}); afterEach(() => { jest.resetModules() });',
+      options: [{ allow: [HookName.afterEach] }],
+      errors: [
+        {
+          messageId: 'unexpectedHook',
+          data: { hookName: HookName.beforeEach },
+        },
+      ],
+    },
+  ],
+});
