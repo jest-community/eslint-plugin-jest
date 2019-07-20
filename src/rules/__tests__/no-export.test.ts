0f33242c1f94489f200ca30e3696ae20a8322846
Gareth Jones <Jones258@Gmail.com>
2019-07-20 22:56:04 +1200

    chore(no-export): convert to typescript (#323)

--- src/rules/__tests__/no-export.test.ts
+++ src/rules/__tests__/no-export.test.ts
@@ -1,7 +1,7 @@
-import { RuleTester } from 'eslint';
+import { TSESLint } from '@typescript-eslint/experimental-utils';
 import rule from '../no-export';
 
-const ruleTester = new RuleTester({
+const ruleTester = new TSESLint.RuleTester({
   parserOptions: {
     ecmaVersion: 2015,
     sourceType: 'module',
---
+++ src/rules/no-export.ts
@@ -0,0 +1,72 @@
+import {
+  AST_NODE_TYPES,
+  TSESTree,
+} from '@typescript-eslint/experimental-utils';
+import { createRule, isTestCase } from './tsUtils';
+
+export default createRule({
+  name: __filename,
+  meta: {
+    docs: {
+      category: 'Best Practices',
+      description:
+        'Prevents exports from test files. If a file has at least 1 test in it, then this rule will prevent exports.',
+      recommended: false,
+    },
+    messages: {
+      unexpectedExport: `Do not export from a test file.`,
+    },
+    type: 'suggestion',
+    schema: [],
+  },
+  defaultOptions: [],
+  create(context) {
+    const exportNodes: Array<
+      | TSESTree.ExportNamedDeclaration
+      | TSESTree.ExportDefaultDeclaration
+      | TSESTree.MemberExpression
+    > = [];
+    let hasTestCase = false;
+
+    return {
+      'Program:exit'() {
+        if (hasTestCase && exportNodes.length > 0) {
+          for (const node of exportNodes) {
+            context.report({ node, messageId: 'unexpectedExport' });
+          }
+        }
+      },
+
+      CallExpression(node) {
+        if (isTestCase(node)) {
+          hasTestCase = true;
+        }
+      },
+      'ExportNamedDeclaration, ExportDefaultDeclaration'(
+        node:
+          | TSESTree.ExportNamedDeclaration
+          | TSESTree.ExportDefaultDeclaration,
+      ) {
+        exportNodes.push(node);
+      },
+      'AssignmentExpression > MemberExpression'(
+        node: TSESTree.MemberExpression,
+      ) {
+        let { object, property } = node;
+
+        if (object.type === AST_NODE_TYPES.MemberExpression) {
+          ({ object, property } = object);
+        }
+
+        if (
+          'name' in object &&
+          object.name === 'module' &&
+          property.type === AST_NODE_TYPES.Identifier &&
+          /^exports?$/.test(property.name)
+        ) {
+          exportNodes.push(node);
+        }
+      },
+    };
+  },
+});
