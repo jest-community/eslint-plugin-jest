83a0c396a3bd0cd7bad2f1ac8982a62f1fb1166d
Gareth Jones <Jones258@Gmail.com>
2019-07-21 01:44:14 +1200

    chore(consistent-test-it): migrate to TS (#327)

--- src/rules/consistent-test-it.ts
+++ src/rules/consistent-test-it.ts
@@ -1,9 +1,13 @@
-import { getDocsUrl, getNodeName, isDescribe, isTestCase } from './util';
+import { AST_NODE_TYPES } from '@typescript-eslint/experimental-utils';
+import { createRule, getNodeName, isDescribe, isTestCase } from './tsUtils';
 
-export default {
+export default createRule({
+  name: __filename,
   meta: {
     docs: {
-      url: getDocsUrl(__filename),
+      category: 'Best Practices',
+      description: 'Have control over `test` and `it` usages',
+      recommended: false,
     },
     fixable: 'code',
     messages: {
@@ -26,7 +30,14 @@ export default {
         additionalProperties: false,
       },
     ],
+    type: 'suggestion',
   },
+  defaultOptions: [
+    { fn: 'test', withinDescribe: 'it' } as {
+      fn?: 'it' | 'test';
+      withinDescribe?: 'it' | 'test';
+    },
+  ],
   create(context) {
     const configObj = context.options[0] || {};
     const testKeyword = configObj.fn || 'test';
@@ -38,6 +49,10 @@ export default {
     return {
       CallExpression(node) {
         const nodeName = getNodeName(node.callee);
+
+        if (!nodeName) {
+          return;
+        }
 
         if (isDescribe(node)) {
           describeNestingLevel++;
@@ -56,7 +71,7 @@ export default {
             data: { testKeyword, oppositeTestKeyword },
             fix(fixer) {
               const nodeToReplace =
-                node.callee.type === 'MemberExpression'
+                node.callee.type === AST_NODE_TYPES.MemberExpression
                   ? node.callee.object
                   : node.callee;
 
@@ -81,7 +96,7 @@ export default {
             data: { testKeywordWithinDescribe, oppositeTestKeyword },
             fix(fixer) {
               const nodeToReplace =
-                node.callee.type === 'MemberExpression'
+                node.callee.type === AST_NODE_TYPES.MemberExpression
                   ? node.callee.object
                   : node.callee;
 
@@ -101,9 +116,9 @@ export default {
       },
     };
   },
-};
+});
 
-function getPreferredNodeName(nodeName, preferredTestKeyword) {
+function getPreferredNodeName(nodeName: string, preferredTestKeyword: string) {
   switch (nodeName) {
     case 'fit':
       return 'test.only';
@@ -114,7 +129,7 @@ function getPreferredNodeName(nodeName, preferredTestKeyword) {
   }
 }
 
-function getOppositeTestKeyword(test) {
+function getOppositeTestKeyword(test: string) {
   if (test === 'test') {
     return 'it';
   }
