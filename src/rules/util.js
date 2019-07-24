6ab8d30ae163809a545aa4f91a12962c2ad73de0
Gareth Jones <jones258@gmail.com>
2019-07-24 13:36:49 +1000

    chore(util): remove unused functions

--- src/rules/util.js
+++ src/rules/util.js
@@ -97,50 +97,13 @@ export const argument = node =>
 export const argument2 = node =>
   node.parent.parent.parent.arguments && node.parent.parent.parent.arguments[0];
 
-const describeAliases = new Set(['describe', 'fdescribe', 'xdescribe']);
-
-const testCaseNames = new Set(['fit', 'it', 'test', 'xit', 'xtest']);
-
-const describeProperties = new Set(['each', 'only', 'skip']);
-
-const testCaseProperties = new Set(['each', 'only', 'skip', 'todo']);
-
-export const isTestCase = node =>
-  node &&
-  node.type === 'CallExpression' &&
-  ((node.callee.type === 'Identifier' && testCaseNames.has(node.callee.name)) ||
-    (node.callee.type === 'MemberExpression' &&
-      node.callee.object.type === 'Identifier' &&
-      testCaseNames.has(node.callee.object.name) &&
-      node.callee.property.type === 'Identifier' &&
-      testCaseProperties.has(node.callee.property.name)));
-
-export const isDescribe = node =>
-  node &&
-  node.type === 'CallExpression' &&
-  ((node.callee.type === 'Identifier' &&
-    describeAliases.has(node.callee.name)) ||
-    (node.callee.type === 'MemberExpression' &&
-      node.callee.object.type === 'Identifier' &&
-      describeAliases.has(node.callee.object.name) &&
-      node.callee.property.type === 'Identifier' &&
-      describeProperties.has(node.callee.property.name)));
-
 export const isFunction = node =>
   node &&
   (node.type === 'FunctionExpression' ||
     node.type === 'ArrowFunctionExpression');
 
-export const isString = node =>
-  node &&
-  ((node.type === 'Literal' && typeof node.value === 'string') ||
-    isTemplateLiteral(node));
-
 export const isTemplateLiteral = node =>
   node && node.type === 'TemplateLiteral';
-
-export const hasExpressions = node =>
-  node && node.expressions && node.expressions.length > 0;
 
 export const getStringValue = arg =>
   isTemplateLiteral(arg) ? arg.quasis[0].value.raw : arg.value;
