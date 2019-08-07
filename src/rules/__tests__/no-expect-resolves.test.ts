f41d5c4bcaa3fb15377df28f620db7c623635ac8
Eran Shabi <eran514@gmail.com>
2019-08-07 19:18:25 +0300

    feat(rules): no-expect-resolves (#364)

---
+++ src/rules/__tests__/no-expect-resolves.test.ts
@@ -0,0 +1,24 @@
+import { TSESLint } from '@typescript-eslint/experimental-utils';
+import rule from '../no-expect-resolves';
+
+const ruleTester = new TSESLint.RuleTester({
+  parserOptions: {
+    ecmaVersion: 2017,
+  },
+});
+
+ruleTester.run('no-expect-resolves', rule, {
+  valid: [
+    `test('some test', async () => {
+         expect(await Promise.resolve(1)).toBe(1);
+     });`,
+  ],
+  invalid: [
+    {
+      code: `test('some test', async () => {
+             await expect(Promise.resolve(1)).resolves.toBe(1);
+         });`,
+      errors: [{ endColumn: 55, column: 47, messageId: 'expectResolves' }],
+    },
+  ],
+});
---
+++ src/rules/no-expect-resolves.ts
@@ -0,0 +1,40 @@
+import {
+  AST_NODE_TYPES,
+  TSESTree,
+} from '@typescript-eslint/experimental-utils';
+import { createRule, isExpectCall } from './tsUtils';
+
+function isIdentifierResolves(node: TSESTree.MemberExpression) {
+  return (
+    node.property.type === AST_NODE_TYPES.Identifier &&
+    node.property.name === 'resolves'
+  );
+}
+
+function isExpectResolves(node: TSESTree.MemberExpression) {
+  return isExpectCall(node.object) && isIdentifierResolves(node);
+}
+
+export default createRule({
+  name: __filename,
+  meta: {
+    docs: {
+      category: 'Best Practices',
+      description: 'Disallow expect.resolves',
+      recommended: false,
+    },
+    messages: {
+      expectResolves: 'Use `expect(await promise)` instead.',
+    },
+    schema: [],
+    type: 'suggestion',
+  },
+  defaultOptions: [],
+  create: context => ({
+    MemberExpression(node) {
+      if (isExpectResolves(node)) {
+        context.report({ node: node.property, messageId: 'expectResolves' });
+      }
+    },
+  }),
+});
