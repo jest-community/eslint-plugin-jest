5dd474b9d0f4ffbbc55206afa6592cfefa4d4a49
Gareth Jones <jones258@gmail.com>
2019-08-12 20:49:42 +1200

    chore(prefer-called-with): convert to typescript

---
+++ src/rules/prefer-called-with.ts
@@ -0,0 +1,41 @@
+import { createRule, isExpectCall, parseExpectCall } from './tsUtils';
+
+export default createRule({
+  name: __filename,
+  meta: {
+    docs: {
+      category: 'Best Practices',
+      description:
+        'Suggest using `toBeCalledWith()` OR `toHaveBeenCalledWith()`',
+      recommended: false,
+    },
+    messages: {
+      preferCalledWith: 'Prefer {{name}}With(/* expected args */)',
+    },
+    type: 'suggestion',
+    schema: [],
+  },
+  defaultOptions: [],
+  create(context) {
+    return {
+      CallExpression(node) {
+        if (!isExpectCall(node)) {
+          return;
+        }
+
+        const { modifier, matcher } = parseExpectCall(node);
+
+        // Could check resolves/rejects here but not a likely idiom.
+        if (matcher && !modifier) {
+          if (['toBeCalled', 'toHaveBeenCalled'].includes(matcher.name)) {
+            context.report({
+              data: { name: matcher.name }, // todo: rename to 'matcherName'
+              messageId: 'preferCalledWith',
+              node: matcher.node.property,
+            });
+          }
+        }
+      },
+    };
+  },
+});
