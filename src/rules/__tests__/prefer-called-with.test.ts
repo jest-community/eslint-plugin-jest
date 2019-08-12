4ca5889f0a7e05a4e919ec425ad9c47fa68317f2
Gareth Jones <Jones258@Gmail.com>
2019-08-12 21:26:07 +1200

    chore(prefer-called-with): convert to typescript (#385)

--- src/rules/__tests__/prefer-called-with.test.ts
+++ src/rules/__tests__/prefer-called-with.test.ts
@@ -1,7 +1,7 @@
-import { RuleTester } from 'eslint';
+import { TSESLint } from '@typescript-eslint/experimental-utils';
 import rule from '../prefer-called-with';
 
-const ruleTester = new RuleTester();
+const ruleTester = new TSESLint.RuleTester();
 
 ruleTester.run('prefer-called-with', rule, {
   valid: [
