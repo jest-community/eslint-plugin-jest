c446449e32da29667ab81bb19ea226edb18419af
Gareth Jones <Jones258@Gmail.com>
2019-08-15 19:17:42 +1200

    chore(prefer-to-have-length): convert to typescript (#396)

--- src/rules/__tests__/prefer-to-have-length.test.ts
+++ src/rules/__tests__/prefer-to-have-length.test.ts
@@ -1,19 +1,31 @@
-import { RuleTester } from 'eslint';
+import { TSESLint } from '@typescript-eslint/experimental-utils';
 import rule from '../prefer-to-have-length';
 
-const ruleTester = new RuleTester();
+const ruleTester = new TSESLint.RuleTester({
+  parserOptions: {
+    ecmaVersion: 2015,
+  },
+});
 
 ruleTester.run('prefer-to-have-length', rule, {
   valid: [
     'expect(files).toHaveLength(1);',
     "expect(files.name).toBe('file');",
+    "expect(files[`name`]).toBe('file');",
     'expect(result).toBe(true);',
     `expect(user.getUserName(5)).resolves.toEqual('Paul')`,
     `expect(user.getUserName(5)).rejects.toEqual('Paul')`,
     'expect(a);',
+    'expect(files["length"]).toBe(1);',
   ],
 
   invalid: [
+    // todo: support this
+    // {
+    //   code: 'expect(files["length"]).toBe(1);',
+    //   errors: [{ messageId: 'useToHaveLength', column: 22, line: 1 }],
+    //   output: 'expect(files).toHaveLength(1);',
+    // },
     {
       code: 'expect(files.length).toBe(1);',
       errors: [{ messageId: 'useToHaveLength', column: 22, line: 1 }],
