ee23634431b19fda5285474d2e2701bdcbf6910f
Gareth Jones <jones258@gmail.com>
2019-12-01 08:55:32 +1300

    refactor(no-focused-tests): use `isSupportedAccessor` utility function

--- src/rules/no-focused-tests.ts
+++ src/rules/no-focused-tests.ts
@@ -2,7 +2,12 @@ import {
   AST_NODE_TYPES,
   TSESTree,
 } from '@typescript-eslint/experimental-utils';
-import { DescribeAlias, TestCaseName, createRule } from './utils';
+import {
+  DescribeAlias,
+  TestCaseName,
+  createRule,
+  isSupportedAccessor,
+} from './utils';
 
 const testFunctions = new Set<string>([
   DescribeAlias.describe,
@@ -17,14 +22,9 @@ const matchesTestFunction = (object: TSESTree.LeftHandSideExpression) =>
 const isCallToFocusedTestFunction = (object: TSESTree.Identifier) =>
   object.name.startsWith('f') && testFunctions.has(object.name.substring(1));
 
-const isPropertyNamedOnly = (
-  property: TSESTree.Expression | TSESTree.Identifier,
-) =>
-  ('name' in property && property.name === 'only') ||
-  ('value' in property && property.value === 'only');
-
 const isCallToTestOnlyFunction = (callee: TSESTree.MemberExpression) =>
-  matchesTestFunction(callee.object) && isPropertyNamedOnly(callee.property);
+  matchesTestFunction(callee.object) &&
+  isSupportedAccessor(callee.property, 'only');
 
 export default createRule({
   name: __filename,
