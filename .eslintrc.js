d8fe52eba170f40e01a5e378f4e8fb02bd39a230
Gareth Jones <jones258@gmail.com>
2020-06-19 12:42:26 +1200

    chore: add `eslint-plugin-eslint-config`

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ const globals = require('./src/globals.json');
 module.exports = {
   parser: require.resolve('@typescript-eslint/parser'),
   extends: [
+    'plugin:eslint-config/rc',
     'plugin:eslint-plugin/recommended',
     'plugin:eslint-comments/recommended',
     'plugin:node/recommended',
@@ -13,6 +14,7 @@ module.exports = {
     'prettier/@typescript-eslint',
   ],
   plugins: [
+    'eslint-config',
     'eslint-plugin',
     'eslint-comments',
     'node',
