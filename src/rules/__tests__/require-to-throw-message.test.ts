24ea76cd36b36508bab75755a332f2db5108876f
Gareth Jones <jones258@gmail.com>
2020-10-06 11:43:12 +1300

    test(require-to-throw-message): use dedent for all cases

--- src/rules/__tests__/require-to-throw-message.test.ts
+++ src/rules/__tests__/require-to-throw-message.test.ts
@@ -1,4 +1,5 @@
 import { TSESLint } from '@typescript-eslint/experimental-utils';
+import dedent from 'dedent';
 import resolveFrom from 'resolve-from';
 import rule from '../require-to-throw-message';
 
@@ -14,51 +15,59 @@ ruleTester.run('require-to-throw-message', rule, {
     // String
     "expect(() => { throw new Error('a'); }).toThrow('a');",
     "expect(() => { throw new Error('a'); }).toThrowError('a');",
-    `test('string', async () => {
+    dedent`
+      test('string', async () => {
         const throwErrorAsync = async () => { throw new Error('a') };
         await expect(throwErrorAsync()).rejects.toThrow('a');
         await expect(throwErrorAsync()).rejects.toThrowError('a');
-    })`,
+      })
+    `,
 
     // Template literal
     "const a = 'a'; expect(() => { throw new Error('a'); }).toThrow(`${a}`);",
     "const a = 'a'; expect(() => { throw new Error('a'); }).toThrowError(`${a}`);",
-    `test('Template literal', async () => {
+    dedent`
+      test('Template literal', async () => {
         const a = 'a';
         const throwErrorAsync = async () => { throw new Error('a') };
         await expect(throwErrorAsync()).rejects.toThrow(\`\${a}\`);
         await expect(throwErrorAsync()).rejects.toThrowError(\`\${a}\`);
-    })`,
+      })
+    `,
 
     // Regex
     "expect(() => { throw new Error('a'); }).toThrow(/^a$/);",
     "expect(() => { throw new Error('a'); }).toThrowError(/^a$/);",
-    `test('Regex', async () => {
+    dedent`
+      test('Regex', async () => {
         const throwErrorAsync = async () => { throw new Error('a') };
         await expect(throwErrorAsync()).rejects.toThrow(/^a$/);
         await expect(throwErrorAsync()).rejects.toThrowError(/^a$/);
-    })`,
+      })
+    `,
 
     // Function
-    "expect(() => { throw new Error('a'); })" +
-      ".toThrow((() => { return 'a'; })());",
-    "expect(() => { throw new Error('a'); })" +
-      ".toThrowError((() => { return 'a'; })());",
-    `test('Function', async () => {
+    "expect(() => { throw new Error('a'); }).toThrow((() => { return 'a'; })());",
+    "expect(() => { throw new Error('a'); }).toThrowError((() => { return 'a'; })());",
+    dedent`
+      test('Function', async () => {
         const throwErrorAsync = async () => { throw new Error('a') };
         const fn = () => { return 'a'; };
         await expect(throwErrorAsync()).rejects.toThrow(fn());
         await expect(throwErrorAsync()).rejects.toThrowError(fn());
-    })`,
+      })
+    `,
 
     // Allow no message for `not`.
     "expect(() => { throw new Error('a'); }).not.toThrow();",
     "expect(() => { throw new Error('a'); }).not.toThrowError();",
-    `test('Allow no message for "not"', async () => {
+    dedent`
+      test('Allow no message for "not"', async () => {
         const throwErrorAsync = async () => { throw new Error('a') };
         await expect(throwErrorAsync()).resolves.not.toThrow();
         await expect(throwErrorAsync()).resolves.not.toThrowError();
-    })`,
+      })
+    `,
     'expect(a);',
   ],
 
@@ -90,22 +99,24 @@ ruleTester.run('require-to-throw-message', rule, {
 
     // Empty rejects.toThrow / rejects.toThrowError
     {
-      code: `test('empty rejects.toThrow', async () => {
-        const throwErrorAsync = async () => { throw new Error('a') };
-        await expect(throwErrorAsync()).rejects.toThrow();
-        await expect(throwErrorAsync()).rejects.toThrowError();
-    })`,
+      code: dedent`
+        test('empty rejects.toThrow', async () => {
+          const throwErrorAsync = async () => { throw new Error('a') };
+          await expect(throwErrorAsync()).rejects.toThrow();
+          await expect(throwErrorAsync()).rejects.toThrowError();
+        })
+      `,
       errors: [
         {
           messageId: 'addErrorMessage',
           data: { matcherName: 'toThrow' },
-          column: 49,
+          column: 43,
           line: 3,
         },
         {
           messageId: 'addErrorMessage',
           data: { matcherName: 'toThrowError' },
-          column: 49,
+          column: 43,
           line: 4,
         },
       ],
