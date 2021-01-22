0bfb31a983e59c5e70a408d48c8a19b9dfc02632
Gareth Jones <jones258@gmail.com>
2021-01-22 14:34:11 +1300

    feat(unbound-method): add original `unbound-method` rule

---
+++ src/rules/__tests__/unbound-method.test.ts
@@ -0,0 +1,537 @@
+import path from 'path';
+import { ESLintUtils, TSESLint } from '@typescript-eslint/experimental-utils';
+import rule, { MessageIds, Options } from '../unbound-method';
+
+function getFixturesRootDir(): string {
+  return path.join(__dirname, 'fixtures');
+}
+
+const rootPath = getFixturesRootDir();
+
+const ruleTester = new ESLintUtils.RuleTester({
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    tsconfigRootDir: rootPath,
+    project: './tsconfig.json',
+  },
+});
+
+function addContainsMethodsClass(code: string): string {
+  return `
+class ContainsMethods {
+  bound?: () => void;
+  unbound?(): void;
+
+  static boundStatic?: () => void;
+  static unboundStatic?(): void;
+}
+
+let instance = new ContainsMethods();
+
+const arith = {
+  double(this: void, x: number): number {
+    return x * 2;
+  }
+};
+
+${code}
+  `;
+}
+function addContainsMethodsClassInvalid(
+  code: string[],
+): Array<TSESLint.InvalidTestCase<MessageIds, Options>> {
+  return code.map(c => ({
+    code: addContainsMethodsClass(c),
+    errors: [
+      {
+        line: 18,
+        messageId: 'unbound',
+      },
+    ],
+  }));
+}
+
+ruleTester.run('unbound-method', rule, {
+  valid: [
+    'Promise.resolve().then(console.log);',
+    "['1', '2', '3'].map(Number.parseInt);",
+    '[5.2, 7.1, 3.6].map(Math.floor);',
+    'const x = console.log;',
+    ...[
+      'instance.bound();',
+      'instance.unbound();',
+
+      'ContainsMethods.boundStatic();',
+      'ContainsMethods.unboundStatic();',
+
+      'const bound = instance.bound;',
+      'const boundStatic = ContainsMethods;',
+
+      'const { bound } = instance;',
+      'const { boundStatic } = ContainsMethods;',
+
+      '(instance.bound)();',
+      '(instance.unbound)();',
+
+      '(ContainsMethods.boundStatic)();',
+      '(ContainsMethods.unboundStatic)();',
+
+      'instance.bound``;',
+      'instance.unbound``;',
+
+      'if (instance.bound) { }',
+      'if (instance.unbound) { }',
+
+      'if (instance.bound !== undefined) { }',
+      'if (instance.unbound !== undefined) { }',
+
+      'if (ContainsMethods.boundStatic) { }',
+      'if (ContainsMethods.unboundStatic) { }',
+
+      'if (ContainsMethods.boundStatic !== undefined) { }',
+      'if (ContainsMethods.unboundStatic !== undefined) { }',
+
+      'if (ContainsMethods.boundStatic && instance) { }',
+      'if (ContainsMethods.unboundStatic && instance) { }',
+
+      'if (instance.bound || instance) { }',
+      'if (instance.unbound || instance) { }',
+
+      'ContainsMethods.unboundStatic && 0 || ContainsMethods;',
+
+      '(instance.bound || instance) ? 1 : 0',
+      '(instance.unbound || instance) ? 1 : 0',
+
+      'while (instance.bound) { }',
+      'while (instance.unbound) { }',
+
+      'while (instance.bound !== undefined) { }',
+      'while (instance.unbound !== undefined) { }',
+
+      'while (ContainsMethods.boundStatic) { }',
+      'while (ContainsMethods.unboundStatic) { }',
+
+      'while (ContainsMethods.boundStatic !== undefined) { }',
+      'while (ContainsMethods.unboundStatic !== undefined) { }',
+
+      'instance.bound as any;',
+      'ContainsMethods.boundStatic as any;',
+
+      'instance.bound++;',
+      '+instance.bound;',
+      '++instance.bound;',
+      'instance.bound--;',
+      '-instance.bound;',
+      '--instance.bound;',
+      'instance.bound += 1;',
+      'instance.bound -= 1;',
+      'instance.bound *= 1;',
+      'instance.bound /= 1;',
+
+      'instance.bound || 0;',
+      'instance.bound && 0;',
+
+      'instance.bound ? 1 : 0;',
+      'instance.unbound ? 1 : 0;',
+
+      'ContainsMethods.boundStatic++;',
+      '+ContainsMethods.boundStatic;',
+      '++ContainsMethods.boundStatic;',
+      'ContainsMethods.boundStatic--;',
+      '-ContainsMethods.boundStatic;',
+      '--ContainsMethods.boundStatic;',
+      'ContainsMethods.boundStatic += 1;',
+      'ContainsMethods.boundStatic -= 1;',
+      'ContainsMethods.boundStatic *= 1;',
+      'ContainsMethods.boundStatic /= 1;',
+
+      'ContainsMethods.boundStatic || 0;',
+      'instane.boundStatic && 0;',
+
+      'ContainsMethods.boundStatic ? 1 : 0;',
+      'ContainsMethods.unboundStatic ? 1 : 0;',
+
+      "typeof instance.bound === 'function';",
+      "typeof instance.unbound === 'function';",
+
+      "typeof ContainsMethods.boundStatic === 'function';",
+      "typeof ContainsMethods.unboundStatic === 'function';",
+
+      'instance.unbound = () => {};',
+      'instance.unbound = instance.unbound.bind(instance);',
+      'if (!!instance.unbound) {}',
+      'void instance.unbound',
+      'delete instance.unbound',
+
+      'const { double } = arith;',
+    ].map(addContainsMethodsClass),
+    `
+interface RecordA {
+  readonly type: 'A';
+  readonly a: {};
+}
+interface RecordB {
+  readonly type: 'B';
+  readonly b: {};
+}
+type AnyRecord = RecordA | RecordB;
+
+function test(obj: AnyRecord) {
+  switch (obj.type) {
+  }
+}
+    `,
+    // https://github.com/typescript-eslint/typescript-eslint/issues/496
+    `
+class CommunicationError {
+  constructor() {
+    const x = CommunicationError.prototype;
+  }
+}
+    `,
+    `
+class CommunicationError {}
+const x = CommunicationError.prototype;
+    `,
+    // optional chain
+    `
+class ContainsMethods {
+  bound?: () => void;
+  unbound?(): void;
+
+  static boundStatic?: () => void;
+  static unboundStatic?(): void;
+}
+
+function foo(instance: ContainsMethods | null) {
+  instance?.bound();
+  instance?.unbound();
+
+  instance?.bound++;
+
+  if (instance?.bound) {
+  }
+  if (instance?.unbound) {
+  }
+
+  typeof instance?.bound === 'function';
+  typeof instance?.unbound === 'function';
+}
+    `,
+    // https://github.com/typescript-eslint/typescript-eslint/issues/1425
+    `
+interface OptionalMethod {
+  mightBeDefined?(): void;
+}
+
+const x: OptionalMethod = {};
+declare const myCondition: boolean;
+if (myCondition || x.mightBeDefined) {
+  console.log('hello world');
+}
+    `,
+    // https://github.com/typescript-eslint/typescript-eslint/issues/1256
+    `
+class A {
+  unbound(): void {
+    this.unbound = undefined;
+    this.unbound = this.unbound.bind(this);
+  }
+}
+    `,
+    'const { parseInt } = Number;',
+    'const { log } = console;',
+    `
+let parseInt;
+({ parseInt } = Number);
+    `,
+    `
+let log;
+({ log } = console);
+    `,
+    `
+const foo = {
+  bar: 'bar',
+};
+const { bar } = foo;
+    `,
+    `
+class Foo {
+  unbnound() {}
+  bar = 4;
+}
+const { bar } = new Foo();
+    `,
+    `
+class Foo {
+  bound = () => 'foo';
+}
+const { bound } = new Foo();
+    `,
+  ],
+  invalid: [
+    {
+      code: `
+class Console {
+  log(str) {
+    process.stdout.write(str);
+  }
+}
+
+const console = new Console();
+
+Promise.resolve().then(console.log);
+      `,
+      errors: [
+        {
+          line: 10,
+          messageId: 'unbound',
+        },
+      ],
+    },
+    {
+      code: `
+import { console } from './class';
+const x = console.log;
+      `,
+      errors: [
+        {
+          line: 3,
+          messageId: 'unbound',
+        },
+      ],
+    },
+    {
+      code: addContainsMethodsClass(`
+function foo(arg: ContainsMethods | null) {
+  const unbound = arg?.unbound;
+  arg.unbound += 1;
+  arg?.unbound as any;
+}
+      `),
+      errors: [
+        {
+          line: 20,
+          messageId: 'unbound',
+        },
+        {
+          line: 21,
+          messageId: 'unbound',
+        },
+        {
+          line: 22,
+          messageId: 'unbound',
+        },
+      ],
+    },
+    ...addContainsMethodsClassInvalid([
+      'const unbound = instance.unbound;',
+      'const unboundStatic = ContainsMethods.unboundStatic;',
+
+      'const { unbound } = instance;',
+      'const { unboundStatic } = ContainsMethods;',
+
+      '<any>instance.unbound;',
+      'instance.unbound as any;',
+
+      '<any>ContainsMethods.unboundStatic;',
+      'ContainsMethods.unboundStatic as any;',
+
+      'instance.unbound || 0;',
+      'ContainsMethods.unboundStatic || 0;',
+
+      'instance.unbound ? instance.unbound : null',
+    ]),
+    {
+      code: `
+class ContainsMethods {
+  unbound?(): void;
+
+  static unboundStatic?(): void;
+}
+
+new ContainsMethods().unbound;
+
+ContainsMethods.unboundStatic;
+      `,
+      options: [
+        {
+          ignoreStatic: true,
+        },
+      ],
+      errors: [
+        {
+          line: 8,
+          messageId: 'unbound',
+        },
+      ],
+    },
+    // https://github.com/typescript-eslint/typescript-eslint/issues/496
+    {
+      code: `
+class CommunicationError {
+  foo() {}
+}
+const x = CommunicationError.prototype.foo;
+      `,
+      errors: [
+        {
+          line: 5,
+          messageId: 'unbound',
+        },
+      ],
+    },
+    {
+      // Promise.all is not auto-bound to Promise
+      code: 'const x = Promise.all;',
+      errors: [
+        {
+          line: 1,
+          messageId: 'unbound',
+        },
+      ],
+    },
+    {
+      code: `
+class Foo {
+  unbound() {}
+}
+const instance = new Foo();
+
+let x;
+
+x = instance.unbound; // THIS SHOULD ERROR
+instance.unbound = x; // THIS SHOULD NOT
+      `,
+      errors: [
+        {
+          line: 9,
+          messageId: 'unbound',
+        },
+      ],
+    },
+    {
+      code: `
+class Foo {
+  unbound = function () {};
+}
+const unbound = new Foo().unbound;
+      `,
+      errors: [
+        {
+          line: 5,
+          messageId: 'unbound',
+        },
+      ],
+    },
+    {
+      code: `
+class Foo {
+  unbound() {}
+}
+const { unbound } = new Foo();
+      `,
+      errors: [
+        {
+          line: 5,
+          messageId: 'unbound',
+        },
+      ],
+    },
+    {
+      code: `
+class Foo {
+  unbound = function () {};
+}
+const { unbound } = new Foo();
+      `,
+      errors: [
+        {
+          line: 5,
+          messageId: 'unbound',
+        },
+      ],
+    },
+    {
+      code: `
+class Foo {
+  unbound() {}
+}
+let unbound;
+({ unbound } = new Foo());
+      `,
+      errors: [
+        {
+          line: 6,
+          messageId: 'unbound',
+        },
+      ],
+    },
+    {
+      code: `
+class Foo {
+  unbound = function () {};
+}
+let unbound;
+({ unbound } = new Foo());
+      `,
+      errors: [
+        {
+          line: 6,
+          messageId: 'unbound',
+        },
+      ],
+    },
+    {
+      code: `
+class CommunicationError {
+  foo() {}
+}
+const { foo } = CommunicationError.prototype;
+      `,
+      errors: [
+        {
+          line: 5,
+          messageId: 'unbound',
+        },
+      ],
+    },
+    {
+      code: `
+class CommunicationError {
+  foo() {}
+}
+let foo;
+({ foo } = CommunicationError.prototype);
+      `,
+      errors: [
+        {
+          line: 6,
+          messageId: 'unbound',
+        },
+      ],
+    },
+    {
+      code: `
+import { console } from './class';
+const { log } = console;
+      `,
+      errors: [
+        {
+          line: 3,
+          messageId: 'unbound',
+        },
+      ],
+    },
+    {
+      code: 'const { all } = Promise;',
+      errors: [
+        {
+          line: 1,
+          messageId: 'unbound',
+        },
+      ],
+    },
+  ],
+});
---
+++ src/rules/unbound-method.ts
@@ -0,0 +1,358 @@
+import {
+  ASTUtils,
+  AST_NODE_TYPES,
+  ESLintUtils,
+  TSESTree,
+} from '@typescript-eslint/experimental-utils';
+import * as ts from 'typescript';
+import { createRule } from './utils';
+
+const tsutils = {
+  hasModifier(
+    modifiers: ts.ModifiersArray | undefined,
+    ...kinds: Array<ts.Modifier['kind']>
+  ) {
+    if (modifiers === undefined) {
+      return false;
+    }
+
+    for (const modifier of modifiers) {
+      if (kinds.includes(modifier.kind)) {
+        return true;
+      }
+    }
+
+    return false;
+  },
+};
+
+const util = {
+  createRule,
+  getParserServices: ESLintUtils.getParserServices,
+  isIdentifier: ASTUtils.isIdentifier,
+};
+
+//------------------------------------------------------------------------------
+// Rule Definition
+//------------------------------------------------------------------------------
+
+interface Config {
+  ignoreStatic: boolean;
+}
+
+export type Options = [Config];
+
+export type MessageIds = 'unbound';
+
+/**
+ * The following is a list of exceptions to the rule
+ * Generated via the following script.
+ * This is statically defined to save making purposely invalid calls every lint run
+ * ```
+ SUPPORTED_GLOBALS.flatMap(namespace => {
+  const object = window[namespace];
+    return Object.getOwnPropertyNames(object)
+      .filter(
+        name =>
+          !name.startsWith('_') &&
+          typeof object[name] === 'function',
+      )
+      .map(name => {
+        try {
+          const x = object[name];
+          x();
+        } catch (e) {
+          if (e.message.includes("called on non-object")) {
+            return `${namespace}.${name}`;
+          }
+        }
+      });
+}).filter(Boolean);
+ * ```
+ */
+const nativelyNotBoundMembers = new Set([
+  'Promise.all',
+  'Promise.race',
+  'Promise.resolve',
+  'Promise.reject',
+  'Promise.allSettled',
+  'Object.defineProperties',
+  'Object.defineProperty',
+  'Reflect.defineProperty',
+  'Reflect.deleteProperty',
+  'Reflect.get',
+  'Reflect.getOwnPropertyDescriptor',
+  'Reflect.getPrototypeOf',
+  'Reflect.has',
+  'Reflect.isExtensible',
+  'Reflect.ownKeys',
+  'Reflect.preventExtensions',
+  'Reflect.set',
+  'Reflect.setPrototypeOf',
+]);
+const SUPPORTED_GLOBALS = [
+  'Number',
+  'Object',
+  'String',
+  'RegExp',
+  'Symbol',
+  'Array',
+  'Proxy',
+  'Date',
+  'Infinity',
+  'Atomics',
+  'Reflect',
+  'console',
+  'Math',
+  'JSON',
+  'Intl',
+] as const;
+const nativelyBoundMembers = SUPPORTED_GLOBALS.map(namespace => {
+  if (!(namespace in global)) {
+    // node.js might not have namespaces like Intl depending on compilation options
+    // https://nodejs.org/api/intl.html#intl_options_for_building_node_js
+    return [];
+  }
+  const object = global[namespace];
+
+  return Object.getOwnPropertyNames(object)
+    .filter(
+      name =>
+        !name.startsWith('_') &&
+        typeof (object as Record<string, unknown>)[name] === 'function',
+    )
+    .map(name => `${namespace}.${name}`);
+})
+  .reduce((arr, names) => arr.concat(names), [])
+  .filter(name => !nativelyNotBoundMembers.has(name));
+
+const isNotImported = (
+  symbol: ts.Symbol,
+  currentSourceFile: ts.SourceFile | undefined,
+): boolean => {
+  const { valueDeclaration } = symbol;
+
+  if (!valueDeclaration) {
+    // working around https://github.com/microsoft/TypeScript/issues/31294
+    return false;
+  }
+
+  return (
+    !!currentSourceFile &&
+    currentSourceFile !== valueDeclaration.getSourceFile()
+  );
+};
+
+const getNodeName = (node: TSESTree.Node): string | null =>
+  node.type === AST_NODE_TYPES.Identifier ? node.name : null;
+
+const getMemberFullName = (node: TSESTree.MemberExpression): string =>
+  `${getNodeName(node.object)}.${getNodeName(node.property)}`;
+
+export default util.createRule<Options, MessageIds>({
+  name: 'unbound-method',
+  meta: {
+    docs: {
+      category: 'Best Practices',
+      description:
+        'Enforces unbound methods are called with their expected scope',
+      recommended: 'error',
+      requiresTypeChecking: true,
+    },
+    messages: {
+      unbound:
+        'Avoid referencing unbound methods which may cause unintentional scoping of `this`.',
+    },
+    schema: [
+      {
+        type: 'object',
+        properties: {
+          ignoreStatic: {
+            type: 'boolean',
+          },
+        },
+        additionalProperties: false,
+      },
+    ],
+    type: 'problem',
+  },
+  defaultOptions: [
+    {
+      ignoreStatic: false,
+    },
+  ],
+  create(context, [{ ignoreStatic }]) {
+    const parserServices = util.getParserServices(context);
+    const checker = parserServices.program.getTypeChecker();
+    const currentSourceFile = parserServices.program.getSourceFile(
+      context.getFilename(),
+    );
+
+    return {
+      MemberExpression(node: TSESTree.MemberExpression): void {
+        if (isSafeUse(node)) {
+          return;
+        }
+
+        const objectSymbol = checker.getSymbolAtLocation(
+          parserServices.esTreeNodeToTSNodeMap.get(node.object),
+        );
+
+        if (
+          objectSymbol &&
+          nativelyBoundMembers.includes(getMemberFullName(node)) &&
+          isNotImported(objectSymbol, currentSourceFile)
+        ) {
+          return;
+        }
+
+        const originalNode = parserServices.esTreeNodeToTSNodeMap.get(node);
+        const symbol = checker.getSymbolAtLocation(originalNode);
+
+        if (symbol && isDangerousMethod(symbol, ignoreStatic)) {
+          context.report({
+            messageId: 'unbound',
+            node,
+          });
+        }
+      },
+      'VariableDeclarator, AssignmentExpression'(
+        node: TSESTree.VariableDeclarator | TSESTree.AssignmentExpression,
+      ): void {
+        const [idNode, initNode] =
+          node.type === AST_NODE_TYPES.VariableDeclarator
+            ? [node.id, node.init]
+            : [node.left, node.right];
+
+        if (initNode && idNode.type === AST_NODE_TYPES.ObjectPattern) {
+          const tsNode = parserServices.esTreeNodeToTSNodeMap.get(initNode);
+          const rightSymbol = checker.getSymbolAtLocation(tsNode);
+          const initTypes = checker.getTypeAtLocation(tsNode);
+
+          const notImported =
+            rightSymbol && isNotImported(rightSymbol, currentSourceFile);
+
+          idNode.properties.forEach(property => {
+            if (
+              property.type === AST_NODE_TYPES.Property &&
+              property.key.type === AST_NODE_TYPES.Identifier
+            ) {
+              if (
+                notImported &&
+                util.isIdentifier(initNode) &&
+                nativelyBoundMembers.includes(
+                  `${initNode.name}.${property.key.name}`,
+                )
+              ) {
+                return;
+              }
+
+              const symbol = initTypes.getProperty(property.key.name);
+
+              if (symbol && isDangerousMethod(symbol, ignoreStatic)) {
+                context.report({
+                  messageId: 'unbound',
+                  node,
+                });
+              }
+            }
+          });
+        }
+      },
+    };
+  },
+});
+
+function isDangerousMethod(symbol: ts.Symbol, ignoreStatic: boolean): boolean {
+  const { valueDeclaration } = symbol;
+
+  if (!valueDeclaration) {
+    // working around https://github.com/microsoft/TypeScript/issues/31294
+    return false;
+  }
+
+  switch (valueDeclaration.kind) {
+    case ts.SyntaxKind.PropertyDeclaration:
+      return (
+        (valueDeclaration as ts.PropertyDeclaration).initializer?.kind ===
+        ts.SyntaxKind.FunctionExpression
+      );
+    case ts.SyntaxKind.MethodDeclaration:
+    case ts.SyntaxKind.MethodSignature: {
+      const decl = valueDeclaration as
+        | ts.MethodDeclaration
+        | ts.MethodSignature;
+      const [firstParam] = decl.parameters;
+      const thisArgIsVoid =
+        firstParam?.name.kind === ts.SyntaxKind.Identifier &&
+        firstParam?.name.escapedText === 'this' &&
+        firstParam?.type?.kind === ts.SyntaxKind.VoidKeyword;
+
+      return (
+        !thisArgIsVoid &&
+        !(
+          ignoreStatic &&
+          tsutils.hasModifier(
+            valueDeclaration.modifiers,
+            ts.SyntaxKind.StaticKeyword,
+          )
+        )
+      );
+    }
+  }
+
+  return false;
+}
+
+function isSafeUse(node: TSESTree.Node): boolean {
+  const { parent } = node;
+
+  switch (parent?.type) {
+    case AST_NODE_TYPES.IfStatement:
+    case AST_NODE_TYPES.ForStatement:
+    case AST_NODE_TYPES.MemberExpression:
+    case AST_NODE_TYPES.SwitchStatement:
+    case AST_NODE_TYPES.UpdateExpression:
+    case AST_NODE_TYPES.WhileStatement:
+      return true;
+
+    case AST_NODE_TYPES.CallExpression:
+      return parent.callee === node;
+
+    case AST_NODE_TYPES.ConditionalExpression:
+      return parent.test === node;
+
+    case AST_NODE_TYPES.TaggedTemplateExpression:
+      return parent.tag === node;
+
+    case AST_NODE_TYPES.UnaryExpression:
+      // the first case is safe for obvious
+      // reasons. The second one is also fine
+      // since we're returning something falsy
+      return ['typeof', '!', 'void', 'delete'].includes(parent.operator);
+
+    case AST_NODE_TYPES.BinaryExpression:
+      return ['instanceof', '==', '!=', '===', '!=='].includes(parent.operator);
+
+    case AST_NODE_TYPES.AssignmentExpression:
+      return parent.operator === '=' && node === parent.left;
+
+    case AST_NODE_TYPES.ChainExpression:
+    case AST_NODE_TYPES.TSNonNullExpression:
+    case AST_NODE_TYPES.TSAsExpression:
+    case AST_NODE_TYPES.TSTypeAssertion:
+      return isSafeUse(parent);
+
+    case AST_NODE_TYPES.LogicalExpression:
+      if (parent.operator === '&&' && parent.left === node) {
+        // this is safe, as && will return the left if and only if it's falsy
+        return true;
+      }
+
+      // in all other cases, it's likely the logical expression will return the method ref
+      // so make sure the parent is a safe usage
+      return isSafeUse(parent);
+  }
+
+  return false;
+}
