88f896b10ca3130499a2a5e3b603d33285110b5c
Gareth Jones <jones258@gmail.com>
2021-04-11 13:20:40 +1200

    test(no-test-return-statement): add cases for `each()()`

--- src/rules/__tests__/no-test-return-statement.test.ts
+++ src/rules/__tests__/no-test-return-statement.test.ts
@@ -70,7 +70,23 @@ ruleTester.run('no-test-return-statement', rule, {
     },
     {
       code: dedent`
+        it.each()("one", function () {
+          return expect(1).toBe(1);
+        });
+      `,
+      errors: [{ messageId: 'noReturnValue', column: 3, line: 2 }],
+    },
+    {
+      code: dedent`
         it.only.each\`\`("one", function () {
+          return expect(1).toBe(1);
+        });
+      `,
+      errors: [{ messageId: 'noReturnValue', column: 3, line: 2 }],
+    },
+    {
+      code: dedent`
+        it.only.each()("one", function () {
           return expect(1).toBe(1);
         });
       `,
