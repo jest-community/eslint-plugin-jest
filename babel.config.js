637249b5522997c4d6e84dd1b884803ce321785e
Simen Bekkhus <sbekkhus91@gmail.com>
2021-09-13 12:41:01 +0200

    chore: get node version used in transforms from package.json

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,16 @@
 'use strict';
 
+const semver = require('semver');
+const pkg = require('./package.json');
+
+const supportedNodeVersion = semver.minVersion(pkg.engines.node).version;
+
 // todo: https://github.com/babel/babel/issues/8529 :'(
 module.exports = {
   plugins: ['replace-ts-export-assignment'],
   presets: [
     '@babel/preset-typescript',
-    ['@babel/preset-env', { targets: { node: 10 } }],
+    ['@babel/preset-env', { targets: { node: supportedNodeVersion } }],
   ],
   ignore: ['src/**/__tests__/fixtures/**'],
 };
