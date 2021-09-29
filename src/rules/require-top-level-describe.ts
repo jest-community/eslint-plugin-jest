14a2d1391c9f6f52509316542f45df35853c9b79
Gareth Jones <Jones258@Gmail.com>
2021-09-29 20:36:41 +1300

    feat(require-top-level-describe): support enforcing max num of describes (#912)

--- src/rules/require-top-level-describe.ts
+++ src/rules/require-top-level-describe.ts
@@ -1,7 +1,17 @@
 import { TSESTree } from '@typescript-eslint/experimental-utils';
 import { createRule, isDescribeCall, isHook, isTestCaseCall } from './utils';
 
-export default createRule({
+const messages = {
+  tooManyDescribes:
+    'There should not be more than {{ max }} describe{{ s }} at the top level',
+  unexpectedTestCase: 'All test cases must be wrapped in a describe block.',
+  unexpectedHook: 'All hooks must be wrapped in a describe block.',
+};
+
+export default createRule<
+  [Partial<{ maxNumberOfTopLevelDescribes: number }>],
+  keyof typeof messages
+>({
   name: __filename,
   meta: {
     docs: {
@@ -10,21 +20,46 @@ export default createRule({
         'Require test cases and hooks to be inside a `describe` block',
       recommended: false,
     },
-    messages: {
-      unexpectedTestCase: 'All test cases must be wrapped in a describe block.',
-      unexpectedHook: 'All hooks must be wrapped in a describe block.',
-    },
+    messages,
     type: 'suggestion',
-    schema: [],
+    schema: [
+      {
+        type: 'object',
+        properties: {
+          maxNumberOfTopLevelDescribes: {
+            type: 'number',
+            minimum: 1,
+          },
+        },
+        additionalProperties: false,
+      },
+    ],
   },
-  defaultOptions: [],
+  defaultOptions: [{}],
   create(context) {
+    const { maxNumberOfTopLevelDescribes = Infinity } =
+      context.options[0] ?? {};
+    let numberOfTopLevelDescribeBlocks = 0;
     let numberOfDescribeBlocks = 0;
 
     return {
       CallExpression(node) {
         if (isDescribeCall(node)) {
           numberOfDescribeBlocks++;
+
+          if (numberOfDescribeBlocks === 1) {
+            numberOfTopLevelDescribeBlocks++;
+            if (numberOfTopLevelDescribeBlocks > maxNumberOfTopLevelDescribes) {
+              context.report({
+                node,
+                messageId: 'tooManyDescribes',
+                data: {
+                  max: maxNumberOfTopLevelDescribes,
+                  s: maxNumberOfTopLevelDescribes === 1 ? '' : 's',
+                },
+              });
+            }
+          }
 
           return;
         }
