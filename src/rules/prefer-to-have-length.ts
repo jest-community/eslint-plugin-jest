3bf9fb9a2c89b80214f9dfc5d9639b378535d38c
Gareth Jones <jones258@gmail.com>
2021-12-29 10:48:11 +1300

    refactor(prefer-to-have-length): simplify fixer

--- src/rules/prefer-to-have-length.ts
+++ src/rules/prefer-to-have-length.ts
@@ -48,43 +48,13 @@ export default createRule({
 
         context.report({
           fix(fixer) {
-            const accessorStartToken = context
-              .getSourceCode()
-              .getFirstTokenBetween(
-                argument.object,
-                argument.property,
-                token => token.value === '.' || token.value === '[',
-              );
-
-            /* istanbul ignore if */
-            if (accessorStartToken === null) {
-              throw new Error(
-                `Unexpected null when attempting to fix ${context.getFilename()} - please file a github issue at https://github.com/jest-community/eslint-plugin-jest`,
-              );
-            }
-
-            const fixers = [
-              fixer.remove(accessorStartToken),
-              fixer.remove(argument.property),
+            return [
+              fixer.removeRange([
+                argument.property.range[0] - 1,
+                argument.range[1],
+              ]),
               fixer.replaceText(matcher.node.property, 'toHaveLength'),
             ];
-
-            if (accessorStartToken.value === '[') {
-              const accessorStopToken = context
-                .getSourceCode()
-                .getTokenAfter(argument.property);
-
-              /* istanbul ignore if */
-              if (accessorStopToken === null) {
-                throw new Error(
-                  `Unexpected null when attempting to fix ${context.getFilename()} - please file a github issue at https://github.com/jest-community/eslint-plugin-jest`,
-                );
-              }
-
-              fixers.push(fixer.remove(accessorStopToken));
-            }
-
-            return fixers;
           },
           messageId: 'useToHaveLength',
           node: matcher.node.property,
