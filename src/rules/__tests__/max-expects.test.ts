5b6fd20b37baee87779c9aef856f747e55e0f467
Makoto Tateno <makoto.ttn@gmail.com>
2022-07-15 04:18:03 +0900

    feat: create `max-expects` rule (#1166)
    * feat: create max-expects
    
    * fix: fix test
    
    * chore: update README
    
    * fix: set proper minimum
    
    * refactor: remove unnecessary functions
    
    * refactor: reduce if
    
    * refactor: remove unnecessary type check
    
    * fix: incorrect error reported in case of multiple test blocks
    
    * chore: typo
    
    * refactor: remove unnecessary type check
    
    * chore: remove unneeded return
    
    Co-authored-by: Gareth Jones <jones258@gmail.com>

---
+++ src/rules/__tests__/max-expects.test.ts
@@ -0,0 +1,284 @@
+import { TSESLint } from '@typescript-eslint/utils';
+import dedent from 'dedent';
+import rule from '../max-expects';
+import { espreeParser } from './test-utils';
+
+const ruleTester = new TSESLint.RuleTester({
+  parser: espreeParser,
+  parserOptions: {
+    ecmaVersion: 2017,
+  },
+});
+
+ruleTester.run('max-expects', rule, {
+  valid: [
+    `test('should pass')`,
+    `test('should pass', () => {})`,
+    `test.skip('should pass', () => {})`,
+    dedent`
+      test('should pass', function () {
+        expect(true).toBeDefined();
+      });
+    `,
+    dedent`
+      test('should pass', () => {
+        expect(true).toBeDefined();
+        expect(true).toBeDefined();
+        expect(true).toBeDefined();
+        expect(true).toBeDefined();
+        expect(true).toBeDefined();
+      });
+    `,
+    dedent`
+      test('should pass', () => {
+        expect(true).toBeDefined();
+        expect(true).toBeDefined();
+        expect(true).toBeDefined();
+        expect(true).toBeDefined();
+        expect(true).toBeDefined();
+        // expect(true).toBeDefined();
+      });
+    `,
+    dedent`
+      it('should pass', () => {
+        expect(true).toBeDefined();
+        expect(true).toBeDefined();
+        expect(true).toBeDefined();
+        expect(true).toBeDefined();
+        expect(true).toBeDefined();
+      });
+    `,
+    dedent`
+      test('should pass', async () => {
+        expect(true).toBeDefined();
+        expect(true).toBeDefined();
+        expect(true).toBeDefined();
+        expect(true).toBeDefined();
+        expect(true).toBeDefined();
+      });
+    `,
+    dedent`
+      describe('test', () => {
+        test('should pass', () => {
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+        });
+      });
+    `,
+    dedent`
+      test.each(['should', 'pass'], () => {
+        expect(true).toBeDefined();
+        expect(true).toBeDefined();
+        expect(true).toBeDefined();
+        expect(true).toBeDefined();
+        expect(true).toBeDefined();
+      });
+    `,
+    dedent`
+      test('should pass', () => {
+        expect(true).toBeDefined();
+        expect(true).toBeDefined();
+        expect(true).toBeDefined();
+      });
+      test('should pass', () => {
+        expect(true).toBeDefined();
+        expect(true).toBeDefined();
+        expect(true).toBeDefined();
+      });
+    `,
+    {
+      code: dedent`
+        test('should pass', () => {
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+        });
+      `,
+      options: [
+        {
+          max: 10,
+        },
+      ],
+    },
+  ],
+  invalid: [
+    {
+      code: dedent`
+        test('should not pass', function () {
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+        });
+      `,
+      errors: [
+        {
+          messageId: 'exceededMaxAssertion',
+          line: 7,
+          column: 3,
+        },
+      ],
+    },
+    {
+      code: dedent`
+        test('should not pass', () => {
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+        });
+      `,
+      errors: [
+        {
+          messageId: 'exceededMaxAssertion',
+          line: 7,
+          column: 3,
+        },
+      ],
+    },
+    {
+      code: dedent`
+        it('should not pass', () => {
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+        });
+      `,
+      errors: [
+        {
+          messageId: 'exceededMaxAssertion',
+          line: 7,
+          column: 3,
+        },
+      ],
+    },
+    {
+      code: dedent`
+        it('should not pass', async () => {
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+        });
+      `,
+      errors: [
+        {
+          messageId: 'exceededMaxAssertion',
+          line: 7,
+          column: 3,
+        },
+      ],
+    },
+    {
+      code: dedent`
+        test('should not pass', () => {
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+        });
+        test('should not pass', () => {
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+        });
+      `,
+      errors: [
+        {
+          messageId: 'exceededMaxAssertion',
+          line: 7,
+          column: 3,
+        },
+        {
+          messageId: 'exceededMaxAssertion',
+          line: 15,
+          column: 3,
+        },
+      ],
+    },
+    {
+      code: dedent`
+        describe('test', () => {
+          test('should not pass', () => {
+            expect(true).toBeDefined();
+            expect(true).toBeDefined();
+            expect(true).toBeDefined();
+            expect(true).toBeDefined();
+            expect(true).toBeDefined();
+            expect(true).toBeDefined();
+          });
+        });
+      `,
+      errors: [
+        {
+          messageId: 'exceededMaxAssertion',
+          line: 8,
+          column: 5,
+        },
+      ],
+    },
+    {
+      code: dedent`
+        test.each(['should', 'not', 'pass'], () => {
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+        });
+      `,
+      errors: [
+        {
+          messageId: 'exceededMaxAssertion',
+          line: 7,
+          column: 3,
+        },
+      ],
+    },
+    {
+      code: dedent`
+        test('should not pass', () => {
+          expect(true).toBeDefined();
+          expect(true).toBeDefined();
+        });
+      `,
+      options: [
+        {
+          max: 1,
+        },
+      ],
+      errors: [
+        {
+          messageId: 'exceededMaxAssertion',
+          line: 3,
+          column: 3,
+        },
+      ],
+    },
+  ],
+});
---
+++ src/rules/max-expects.ts
@@ -0,0 +1,69 @@
+import { AST_NODE_TYPES } from '@typescript-eslint/utils';
+import {
+  FunctionExpression,
+  createRule,
+  isExpectCall,
+  isTypeOfJestFnCall,
+} from './utils';
+
+export default createRule({
+  name: __filename,
+  meta: {
+    docs: {
+      category: 'Best Practices',
+      description: 'Enforces a maximum number assertion calls in a test body',
+      recommended: false,
+    },
+    messages: {
+      exceededMaxAssertion:
+        'Too many assertion calls ({{ count }}). Maximum allowed is {{ max }}.',
+    },
+    type: 'suggestion',
+    schema: [
+      {
+        type: 'object',
+        properties: {
+          max: {
+            type: 'integer',
+            minimum: 1,
+          },
+        },
+        additionalProperties: false,
+      },
+    ],
+  },
+  defaultOptions: [{ max: 5 }],
+  create(context, [{ max }]) {
+    let count = 0;
+
+    const onFunctionExpressionEnter = (node: FunctionExpression) => {
+      const isTestFn =
+        node.parent?.type !== AST_NODE_TYPES.CallExpression ||
+        isTypeOfJestFnCall(node.parent, context, ['test']);
+
+      if (isTestFn) {
+        count = 0;
+      }
+    };
+
+    return {
+      FunctionExpression: onFunctionExpressionEnter,
+      ArrowFunctionExpression: onFunctionExpressionEnter,
+      CallExpression(node) {
+        if (!isExpectCall(node)) {
+          return;
+        }
+
+        count += 1;
+
+        if (count > max) {
+          context.report({
+            node,
+            messageId: 'exceededMaxAssertion',
+            data: { count, max },
+          });
+        }
+      },
+    };
+  },
+});
