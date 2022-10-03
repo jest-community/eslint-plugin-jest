b8e61b192ac79971575a3a250df2c54056eadc90
Gareth Jones <Jones258@Gmail.com>
2022-10-04 10:06:28 +1300

    feat: create `no-restricted-jest-methods` rule (#1257)

---
+++ src/rules/__tests__/no-restricted-jest-methods.test.ts
@@ -0,0 +1,110 @@
+import { TSESLint } from '@typescript-eslint/utils';
+import dedent from 'dedent';
+import rule from '../no-restricted-jest-methods';
+import { espreeParser } from './test-utils';
+
+const ruleTester = new TSESLint.RuleTester({
+  parser: espreeParser,
+  parserOptions: {
+    ecmaVersion: 2017,
+  },
+});
+
+ruleTester.run('no-restricted-jest-methods', rule, {
+  valid: [
+    'jest',
+    'jest.mock()',
+    'expect(a).rejects;',
+    'expect(a);',
+    {
+      code: dedent`
+        import { jest } from '@jest/globals';
+
+        jest;
+      `,
+      parserOptions: { sourceType: 'module' },
+    },
+  ],
+  invalid: [
+    {
+      code: 'jest.fn()',
+      options: [{ fn: null }],
+      errors: [
+        {
+          messageId: 'restrictedJestMethod',
+          data: {
+            message: null,
+            restriction: 'fn',
+          },
+          column: 6,
+          line: 1,
+        },
+      ],
+    },
+    {
+      code: 'jest["fn"]()',
+      options: [{ fn: null }],
+      errors: [
+        {
+          messageId: 'restrictedJestMethod',
+          data: {
+            message: null,
+            restriction: 'fn',
+          },
+          column: 6,
+          line: 1,
+        },
+      ],
+    },
+    {
+      code: 'jest.mock()',
+      options: [{ mock: 'Do not use mocks' }],
+      errors: [
+        {
+          messageId: 'restrictedJestMethodWithMessage',
+          data: {
+            message: 'Do not use mocks',
+            restriction: 'mock',
+          },
+          column: 6,
+          line: 1,
+        },
+      ],
+    },
+    {
+      code: 'jest["mock"]()',
+      options: [{ mock: 'Do not use mocks' }],
+      errors: [
+        {
+          messageId: 'restrictedJestMethodWithMessage',
+          data: {
+            message: 'Do not use mocks',
+            restriction: 'mock',
+          },
+          column: 6,
+          line: 1,
+        },
+      ],
+    },
+    {
+      code: dedent`
+        import { jest } from '@jest/globals';
+
+        jest.advanceTimersByTime();
+      `,
+      options: [{ advanceTimersByTime: null }],
+      parserOptions: { sourceType: 'module' },
+      errors: [
+        {
+          messageId: 'restrictedJestMethod',
+          data: {
+            message: null,
+            restriction: 'advanceTimersByTime',
+          },
+          column: 6,
+          line: 3,
+        },
+      ],
+    },
+  ],
+});
