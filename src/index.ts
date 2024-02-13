7023ad9d93ee451956e516d87da49d257fc01866
Gareth Jones <jones258@gmail.com>
2024-02-14 07:33:04 +1300

    fix: add meta properties

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
+import {
+  name as packageName,
+  version as packageVersion,
+} from '../package.json';
 import legacy from './legacy';
 
 export default {
+  meta: { name: packageName, version: packageVersion },
   rules: legacy.rules,
   processors: legacy.processors,
 };
