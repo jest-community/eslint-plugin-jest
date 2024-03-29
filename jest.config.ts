e42791cec9deb472018845025cee2a73208d9e91
Gareth Jones <jones258@gmail.com>
2024-03-30 10:45:08 +1300

    test: skip `unbound-method` when using `@typescript-eslint` v6

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from '@jest/types';
+import { version as typescriptESLintPluginVersion } from '@typescript-eslint/eslint-plugin/package.json';
 import { version as eslintVersion } from 'eslint/package.json';
 import * as semver from 'semver';
 
@@ -24,12 +25,14 @@ const config = {
         '<rootDir>/src/rules/__tests__/fixtures/*',
         '<rootDir>/src/rules/__tests__/test-utils.ts',
       ],
+      coveragePathIgnorePatterns: ['/node_modules/'],
     },
     {
       displayName: 'lint',
       runner: 'jest-runner-eslint',
       testMatch: ['<rootDir>/**/*.{js,ts}'],
       testPathIgnorePatterns: ['<rootDir>/lib/.*'],
+      coveragePathIgnorePatterns: ['/node_modules/'],
     },
   ],
 } satisfies Config.InitialOptions;
@@ -38,6 +41,18 @@ if (semver.major(eslintVersion) >= 9) {
   config.projects = config.projects.filter(
     ({ displayName }) => displayName !== 'lint',
   );
+
+  // jest/unbound-method doesn't work when using @typescript-eslint v6
+  if (semver.major(typescriptESLintPluginVersion) === 6) {
+    for (const project of config.projects) {
+      project.testPathIgnorePatterns.push(
+        '<rootDir>/src/rules/__tests__/unbound-method.test.ts',
+      );
+      project.coveragePathIgnorePatterns.push(
+        '<rootDir>/src/rules/unbound-method.ts',
+      );
+    }
+  }
 }
 
 export default config;
