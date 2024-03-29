44c93c0c1e22bbad65fd167a11d2c6ca00c938f6
Gareth Jones <jones258@gmail.com>
2024-03-30 08:22:36 +1300

    test(prefer-comparison-matcher): deduplicate examples

--- src/rules/__tests__/prefer-comparison-matcher.test.ts
+++ src/rules/__tests__/prefer-comparison-matcher.test.ts
@@ -205,30 +205,30 @@ const generateValidStringLiteralCases = (operator: string, matcher: string) => {
     ['x', "'y'"],
     ['x', '`y`'],
     ['x', '`y${z}`'],
-  ].reduce((cases, [a, b]) => [
-    ...cases,
-    ...[
-      `expect(${a} ${operator} ${b}).${matcher}(true)`,
-      `expect(${a} ${operator} ${b}).${matcher}(false)`,
-      `expect(${a} ${operator} ${b}).not.${matcher}(true)`,
-      `expect(${a} ${operator} ${b}).not.${matcher}(false)`,
-      `expect(${b} ${operator} ${a}).${matcher}(true)`,
-      `expect(${b} ${operator} ${a}).${matcher}(false)`,
-      `expect(${b} ${operator} ${a}).not.${matcher}(true)`,
-      `expect(${b} ${operator} ${a}).not.${matcher}(false)`,
-      `expect(${a} ${operator} ${b}).${matcher}(true)`,
-      `expect(${a} ${operator} ${b}).${matcher}(false)`,
-      `expect(${a} ${operator} ${b}).not.${matcher}(true)`,
-      `expect(${a} ${operator} ${b}).not.${matcher}(false)`,
-      `expect(${b} ${operator} ${a}).${matcher}(true)`,
-      `expect(${b} ${operator} ${a}).${matcher}(false)`,
-      `expect(${b} ${operator} ${a}).not.${matcher}(true)`,
-      `expect(${b} ${operator} ${a}).not.${matcher}(false)`,
-      `expect(${b} ${operator} ${b}).not.${matcher}(false)`,
-      `expect(${b} ${operator} ${b}).resolves.not.${matcher}(false)`,
-      `expect(${b} ${operator} ${b}).resolves.${matcher}(false)`,
+  ].reduce(
+    (cases, [a, b]) => [
+      ...cases,
+      ...[
+        `expect(${a} ${operator} ${b}).${matcher}(true)`,
+        `expect(${a} ${operator} ${b}).${matcher}(false)`,
+        `expect(${a} ${operator} ${b}).not.${matcher}(true)`,
+        `expect(${a} ${operator} ${b}).not.${matcher}(false)`,
+        `expect(${a} ${operator} ${b}).resolves.${matcher}(true)`,
+        `expect(${a} ${operator} ${b}).resolves.${matcher}(false)`,
+        `expect(${a} ${operator} ${b}).resolves.not.${matcher}(true)`,
+        `expect(${a} ${operator} ${b}).resolves.not.${matcher}(false)`,
+        `expect(${b} ${operator} ${a}).resolves.not.${matcher}(false)`,
+        `expect(${b} ${operator} ${a}).resolves.not.${matcher}(true)`,
+        `expect(${b} ${operator} ${a}).resolves.${matcher}(false)`,
+        `expect(${b} ${operator} ${a}).resolves.${matcher}(true)`,
+        `expect(${b} ${operator} ${a}).not.${matcher}(false)`,
+        `expect(${b} ${operator} ${a}).not.${matcher}(true)`,
+        `expect(${b} ${operator} ${a}).${matcher}(false)`,
+        `expect(${b} ${operator} ${a}).${matcher}(true)`,
+      ],
     ],
-  ]);
+    [],
+  );
 };
 
 const testComparisonOperator = (
