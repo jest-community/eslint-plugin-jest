cab1093891ceac01afad3c75709d74de1dbc184c
Gareth Jones <jones258@gmail.com>
2024-04-25 09:19:41 +1200

    test: add more cases for coverage

--- src/rules/__tests__/padding-around-all.test.ts
+++ src/rules/__tests__/padding-around-all.test.ts
@@ -1,4 +1,5 @@
 import type { TSESLint } from '@typescript-eslint/utils';
+import dedent from 'dedent';
 import rule from '../padding-around-all';
 import { FlatCompatRuleTester as RuleTester, espreeParser } from './test-utils';
 
@@ -90,8 +91,139 @@ describe('someText', () => {
 } satisfies TSESLint.InvalidTestCase<'missingPadding', never>;
 
 ruleTester.run('padding-around-all', rule, {
-  valid: [testCase.output],
-  invalid: ['src/component.test.jsx', 'src/component.test.js'].map(
-    filename => ({ ...testCase, filename }),
-  ),
+  valid: [
+    testCase.output,
+    dedent`
+      xyz:
+      afterEach(() => {});
+    `,
+  ],
+  invalid: [
+    ...['src/component.test.jsx', 'src/component.test.js'].map(filename => ({
+      ...testCase,
+      filename,
+    })),
+    {
+      code: dedent`
+        const someText = 'abc'
+        ;afterEach(() => {})
+      `,
+      output: dedent`
+        const someText = 'abc'
+
+        ;afterEach(() => {})
+      `,
+      errors: [
+        {
+          messageId: 'missingPadding',
+          line: 2,
+          column: 2,
+        },
+      ],
+    },
+    {
+      code: dedent`
+        const someText = 'abc';
+        xyz:
+        afterEach(() => {});
+      `,
+      output: dedent`
+        const someText = 'abc';
+
+        xyz:
+        afterEach(() => {});
+      `,
+      errors: [
+        {
+          messageId: 'missingPadding',
+          line: 2,
+          column: 1,
+        },
+      ],
+    },
+    {
+      code: dedent`
+        const expr = 'Papayas';
+        beforeEach(() => {});
+        it('does something?', () => {
+          switch (expr) {
+            case 'Oranges':
+              expect(expr).toBe('Oranges');
+              break;
+            case 'Mangoes':
+            case 'Papayas':
+              const v = 1;
+              expect(v).toBe(1);
+              console.log('Mangoes and papayas are $2.79 a pound.');
+              // Expected output: "Mangoes and papayas are $2.79 a pound."
+              break;
+            default:
+              console.log(\`Sorry, we are out of $\{expr}.\`);
+          }
+        });
+      `,
+      output: dedent`
+        const expr = 'Papayas';
+
+        beforeEach(() => {});
+
+        it('does something?', () => {
+          switch (expr) {
+            case 'Oranges':
+              expect(expr).toBe('Oranges');
+
+              break;
+            case 'Mangoes':
+            case 'Papayas':
+              const v = 1;
+
+              expect(v).toBe(1);
+
+              console.log('Mangoes and papayas are $2.79 a pound.');
+              // Expected output: "Mangoes and papayas are $2.79 a pound."
+              break;
+            default:
+              console.log(\`Sorry, we are out of $\{expr}.\`);
+          }
+        });
+      `,
+      errors: [
+        {
+          messageId: 'missingPadding',
+          line: 2,
+          column: 1,
+          endLine: 2,
+          endColumn: 22,
+        },
+        {
+          messageId: 'missingPadding',
+          line: 3,
+          column: 1,
+          endLine: 18,
+          endColumn: 4,
+        },
+        {
+          messageId: 'missingPadding',
+          line: 7,
+          column: 7,
+          endLine: 7,
+          endColumn: 13,
+        },
+        {
+          messageId: 'missingPadding',
+          line: 11,
+          column: 7,
+          endLine: 11,
+          endColumn: 25,
+        },
+        {
+          messageId: 'missingPadding',
+          line: 12,
+          column: 7,
+          endLine: 12,
+          endColumn: 61,
+        },
+      ],
+    },
+  ],
 });
