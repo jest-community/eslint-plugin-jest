582d48e636e31702503220e5d503b38bb78cbd84
Gareth Jones <jones258@gmail.com>
2024-04-25 08:18:53 +1200

    test: add basic file for `padding-around-all`

---
+++ src/rules/__tests__/padding-around-all.test.ts
@@ -0,0 +1,143 @@
+/**
+ * @fileoverview Enforces single line padding before afterAll blocks
+ */
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+import rule from '../padding-around-all';
+import { FlatCompatRuleTester as RuleTester, espreeParser } from './test-utils';
+
+const ruleTester = new RuleTester({
+  parser: espreeParser,
+  parserOptions: {
+    ecmaVersion: 6,
+  },
+});
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------`
+
+// todo: these should be more fulsome
+const valid = `
+const someText = 'abc';
+
+afterAll(() => {
+});
+
+describe('someText', () => {
+  const something = 'abc';
+
+  // A comment
+  afterAll(() => {
+    // stuff
+  });
+
+  afterAll(() => {
+    // other stuff
+  });
+});
+
+describe('someText', () => {
+  const something = 'abc';
+
+  afterAll(() => {
+    // stuff
+  });
+});
+`;
+
+const invalid = `
+const someText = 'abc';
+afterAll(() => {
+});
+describe('someText', () => {
+  const something = 'abc';
+  // A comment
+  afterAll(() => {
+    // stuff
+  });
+  afterAll(() => {
+    // other stuff
+  });
+});
+
+describe('someText', () => {
+  const something = 'abc';
+  afterAll(() => {
+    // stuff
+  });
+});
+`;
+
+ruleTester.run('padding-around-all', rule, {
+  valid: [valid],
+  invalid: [
+    {
+      filename: 'src/component.test.jsx',
+      code: invalid,
+      output: valid,
+      errors: [
+        {
+          messageId: 'missingPadding',
+          line: 3,
+          column: 1,
+        },
+        {
+          messageId: 'missingPadding',
+          line: 5,
+          column: 1,
+        },
+        {
+          messageId: 'missingPadding',
+          line: 8,
+          column: 3,
+        },
+        {
+          messageId: 'missingPadding',
+          line: 11,
+          column: 3,
+        },
+        {
+          messageId: 'missingPadding',
+          line: 18,
+          column: 3,
+        },
+      ],
+    },
+    {
+      filename: 'src/component.test.js',
+      code: invalid,
+      output: valid,
+      errors: [
+        {
+          messageId: 'missingPadding',
+          line: 3,
+          column: 1,
+        },
+        {
+          messageId: 'missingPadding',
+          line: 5,
+          column: 1,
+        },
+        {
+          messageId: 'missingPadding',
+          line: 8,
+          column: 3,
+        },
+        {
+          messageId: 'missingPadding',
+          line: 11,
+          column: 3,
+        },
+        {
+          messageId: 'missingPadding',
+          line: 18,
+          column: 3,
+        },
+      ],
+    },
+  ],
+});
