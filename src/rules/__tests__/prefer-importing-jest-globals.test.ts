307f6a7b3aad7a5c891d8fea9f115e5d2f4f3fbb
Erin Zimmer <ejzimmer@gmail.com>
2024-08-30 06:01:44 +1000

    fix(prefer-importing-jest-globals): support typescript-eslint parser (#1639)
    * fix(prefer-importing-jest-globals): support new config
    
    * fix(prefer-importing-jest-globals): tests, broken due to types
    
    * fix(prefer-importing-jest-globals): update tests
    
    * fix(prefer-importing-jest-globals): include valid tests
    
    * fix(prefer-importing-jest-globals): split typescript tests
    
    ---------
    
    Co-authored-by: Erin Zimmer <ezimmer@atlassian.com>

--- src/rules/__tests__/prefer-importing-jest-globals.test.ts
+++ src/rules/__tests__/prefer-importing-jest-globals.test.ts
@@ -548,3 +548,61 @@ ruleTester.run('prefer-importing-jest-globals', rule, {
     },
   ],
 });
+
+new RuleTester({
+  parser: require.resolve('@typescript-eslint/parser'),
+}).run('prefer-importing-jest-globals: typescript edition', rule, {
+  valid: [],
+  invalid: [
+    {
+      code: dedent`
+        import describe from '@jest/globals';
+        describe("suite", () => {
+          test("foo");
+          expect(true).toBeDefined();
+        })
+      `,
+      output: dedent`
+        import { describe, expect, test } from '@jest/globals';
+        describe("suite", () => {
+          test("foo");
+          expect(true).toBeDefined();
+        })
+      `,
+      parserOptions: { sourceType: 'module' },
+      errors: [
+        {
+          endColumn: 7,
+          column: 3,
+          line: 3,
+          messageId: 'preferImportingJestGlobal',
+        },
+      ],
+    },
+    {
+      code: dedent`
+        const {describe} = require('@jest/globals');
+        describe("suite", () => {
+          test("foo");
+          expect(true).toBeDefined();
+        })
+      `,
+      output: dedent`
+        const { describe, expect, test } = require('@jest/globals');
+        describe("suite", () => {
+          test("foo");
+          expect(true).toBeDefined();
+        })
+      `,
+      parserOptions: { sourceType: 'script' },
+      errors: [
+        {
+          endColumn: 7,
+          column: 3,
+          line: 3,
+          messageId: 'preferImportingJestGlobal',
+        },
+      ],
+    },
+  ],
+});
