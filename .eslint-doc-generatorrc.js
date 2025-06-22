f375677c7046257e93aeee3122fe21c4565f54de
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2025-06-23 08:02:37 +1200

    chore: run typechecking on js files

--- .eslint-doc-generatorrc.js
+++ .eslint-doc-generatorrc.js
@@ -26,7 +26,11 @@ const config = {
   ],
   ruleListSplit: 'meta.docs.requiresTypeChecking',
   urlConfigs: `https://github.com/jest-community/eslint-plugin-jest/blob/main/README.md#shareable-configurations`,
-  postprocess: doc => format(doc, { ...prettierRC, parser: 'markdown' }),
+  postprocess: doc =>
+    format(doc, {
+      .../** @type {import('prettier').ResolveConfigOptions} */ (prettierRC),
+      parser: 'markdown',
+    }),
 };
 
 module.exports = config;
