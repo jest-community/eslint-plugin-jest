cc30dbb65dd1eaead5e315d7dbb0956036fa4a01
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2025-12-30 12:21:48 +1300

    ci: skip repositories that use a lot of memory when smoke testing (#1909)

--- eslint-remote-tester.config.ts
+++ eslint-remote-tester.config.ts
@@ -6,8 +6,23 @@ import {
 } from 'eslint-remote-tester-repositories';
 import plugin from './src';
 
+// these repos often cause ERR_WORKER_OUT_OF_MEMORY errors, probably because
+// they are large typescript repositories which cause our type-based rules
+// to use a lot of memory
+const reposToIgnore = new Set<string>([
+  'trezor/trezor-suite',
+  'tannerlinsley/react-location',
+  'w-okada/image-analyze-workers',
+  'hubmapconsortium/vitessce',
+  'magiclabs/magic-js',
+  'umbraco/Umbraco.UI',
+  'LaunchMenu/LaunchMenu',
+]);
+
 const config: Config = {
-  repositories: getRepositories({ randomize: true }),
+  repositories: getRepositories({ randomize: true }).filter(repo => {
+    return !reposToIgnore.has(repo);
+  }),
   pathIgnorePattern: getPathIgnorePattern(),
   extensions: ['js', 'jsx', 'ts', 'tsx'],
   concurrentTasks: 3,
