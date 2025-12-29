9319b1671fe843a4d68e6fc2f8b43e37e1712b8f
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2025-12-29 14:01:48 +1300

    ci: run on 100 repos

--- eslint-remote-tester.config.ts
+++ eslint-remote-tester.config.ts
@@ -7,7 +7,7 @@ import {
 import plugin from './src';
 
 const config: Config = {
-  repositories: getRepositories({ randomize: true }).slice(0, 10),
+  repositories: getRepositories({ randomize: true }).slice(0, 100),
   pathIgnorePattern: getPathIgnorePattern(),
   extensions: ['js', 'jsx', 'ts', 'tsx'],
   concurrentTasks: 3,
