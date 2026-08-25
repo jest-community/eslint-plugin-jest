27665cdddb1971ff2c19b0f897fee83112c95462
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2026-08-26 07:06:29 +1200

    fix(prefer-equality-matcher): don't crash on `expect` chain without a value (#2006)

--- src/rules/prefer-equality-matcher.ts
+++ src/rules/prefer-equality-matcher.ts
@@ -48,7 +48,7 @@ export default createRule({
         const matcherArg = getFirstMatcherArg(jestFnCall);
 
         if (
-          comparison.type !== AST_NODE_TYPES.BinaryExpression ||
+          comparison?.type !== AST_NODE_TYPES.BinaryExpression ||
           (comparison.operator !== '===' && comparison.operator !== '!==') ||
           !EqualityMatcher.hasOwnProperty(getAccessorValue(matcher)) ||
           !isBooleanLiteral(matcherArg)
